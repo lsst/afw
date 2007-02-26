c5d4b14c06bc9adb9f07f15331a3973341894763
taxelrod <taxelrod@git.lsstcorp.org>
2007-02-26 22:24:46 +0000

    Initial checkin of Image.h, MaskedImage.h - very incomplete.

---
+++ include/lsst/MaskedImage.h
@@ -0,0 +1,67 @@
+// -*- lsst-c++ -*-
+///////////////////////////////////////////////////////////
+//  MaskedImage.h
+//  Implementation of the Class MaskedImage
+//  Created on:      23-Feb-2007 16:23:06
+//  Original author: Tim Axelrod
+///////////////////////////////////////////////////////////
+
+#ifndef LSST_MASKEDIMAGE_H
+#define LSST_MASKEDIMAGE_H
+
+#include <vw/Image.h>
+#include <vw/Math/BBox.h>
+#include <boost/shared_ptr.hpp>
+#include <list>
+#include <map>
+#include <string>
+
+#include "Mask.h"
+#include "Image.h"
+
+namespace lsst {
+
+    template<typename ImagePixelT, typename MaskPixelT> class PixelProcessingFunc;
+
+    template<class ImagePixelT, class MaskPixelT>
+    class MaskedImage
+    {
+        
+    public:
+        typedef boost::shared_ptr<Image<ImagePixelT> > ImagePtrT;
+        typedef boost::shared_ptr<Mask<MaskPixelT> > MaskPtrT;
+        typedef boost::shared_ptr<MaskedImage<ImagePixelT, MaskPixelT> > MaskedImagePtrT;
+
+	MaskedImage();
+            
+	MaskedImage(ImagePtrT image, MaskPtrT mask);
+
+	MaskedImage processPixels(MaskPixelBooleanFunc<MaskPixelT> selectionFunc, PixelProcessingFunc<ImagePixelT, MaskPixelT> processingFunc);
+            
+	void processPixels(MaskPixelBooleanFunc<MaskPixelT> selectionFunc, PixelProcessingFunc<ImagePixelT, MaskPixelT> processingFunc);
+
+	MaskedImagePtrT getSubImage(BBox2i region);
+
+	void replaceSubImage(BBox2i region, MaskedImage sImage, bool replaceMask, bool replaceAstro);
+
+	void operator+=(const MaskedImage & maskedImageInput);
+
+	void operator-=(const MaskedImage & maskedImageInput);
+
+	void operator*=(const MaskedImage & maskedImageInput);
+
+	void operator/=(const MaskedImage & maskedImageInput);
+
+	ImagePtrT getImage();
+
+	MaskPtrT getMask();
+
+	virtual ~MaskedImage();
+        
+    private:
+	ImagePtrT _image;
+	MaskPtrT _mask;
+    };
+    
+}
+#endif //  LSST_MASKEDIMAGE_H
