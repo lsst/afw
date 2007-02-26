617e5c7ef888e32f459c154aa4b1d1caa45736a3
taxelrod <taxelrod@git.lsstcorp.org>
2007-02-26 23:43:07 +0000

    Initial checkin of MaskedImage_1 test program - doesn't do much yet.

---
+++ tests/MaskedImage_1.cc
@@ -0,0 +1,14 @@
+// -*- lsst-c++ -*-
+#include "lsst/MaskedImage.h"
+
+using namespace lsst;
+
+
+int main()
+{
+     typedef PixelGray<uint8> MaskPixelType;
+     typedef PixelGray<float32> ImagePixelType;
+
+     MaskedImage<ImagePixelType,MaskPixelType > testMaskedImage1();
+     MaskedImage<ImagePixelType,MaskPixelType > testMaskedImage2(272, 1037);
+}
