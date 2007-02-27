2dcd69b59a62be594dcbaf6139f1b4d6d3ed642b
taxelrod <taxelrod@git.lsstcorp.org>
2007-02-27 23:59:32 +0000

    Removed erroneous constructor syntax Added test of += operator

--- tests/MaskedImage_1.cc
+++ tests/MaskedImage_1.cc
@@ -9,6 +9,7 @@ int main()
      typedef PixelGray<uint8> MaskPixelType;
      typedef PixelGray<float32> ImagePixelType;
 
-     MaskedImage<ImagePixelType,MaskPixelType > testMaskedImage1();
+     MaskedImage<ImagePixelType,MaskPixelType > testMaskedImage1(272, 1037);
      MaskedImage<ImagePixelType,MaskPixelType > testMaskedImage2(272, 1037);
+     testMaskedImage2 += testMaskedImage1;
 }
