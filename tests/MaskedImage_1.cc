8b8c7f6728dca39fed98a3c331db8ebc49c398a3
taxelrod <taxelrod@git.lsstcorp.org>
2007-03-03 02:27:39 +0000

    Debugging MaskedImage::processPixels - there is a weird "loss of type" bug which I have not yet figured out.

--- tests/MaskedImage_1.cc
+++ tests/MaskedImage_1.cc
@@ -1,8 +1,9 @@
 // -*- lsst-c++ -*-
 #include "lsst/MaskedImage.h"
+#include <typeinfo>
 
-using namespace lsst;
 
+namespace lsst {
 template <typename ImagePixelT, typename MaskPixelT> class testPixProcFunc : public PixelProcessingFunc<ImagePixelT, MaskPixelT> {
 public:
      typedef typename PixelChannelType<ImagePixelT>::type ImageChannelT;
@@ -12,15 +13,25 @@ public:
      
      void init() {
 	  PixelProcessingFunc<ImagePixelT, MaskPixelT>::_maskPtr->getPlaneBitMask("CR", bitsCR);
+	  testCount = 0;
      }
         
-     bool operator ()(typename PixelProcessingFunc<ImagePixelT, MaskPixelT>::TupleT t) { 
-	  return false;
+     void operator ()(boost::tuple<ImagePixelT&, MaskPixelT&> t) { 
+         //  In general, do something to the pixel values
+	  if (++testCount < 10) std::cout << "yup" << std::endl;
      }
+
+     void foo(int i) {
+	  std::cout << "testPixProcFunc::foo" << std::endl;
+     } 
 private:
-    MaskChannelT bitsCR;
+     MaskChannelT bitsCR;
+     int testCount;
 };
 
+};
+
+using namespace lsst;
 
 int main()
 {
@@ -34,5 +45,15 @@ int main()
      testPixProcFunc<ImagePixelType, MaskPixelType> fooFunc(testMaskedImage1);   // should be a way to automatically convey template types
                                                                                  // from testMaskedImage1 to fooFunc
      fooFunc.init();
+
+     fooFunc.foo(10);
+
+     std::cout << typeid(fooFunc).name() << std::endl;
+
+     ImagePixelType i1(0);
+     MaskPixelType m1(0);
+
+     fooFunc(boost::make_tuple(boost::ref(i1), boost::ref(m1)));
+
      testMaskedImage1.processPixels(fooFunc);
 }
