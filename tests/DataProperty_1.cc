9e94cfc38bcb1bd8cac874112e01bcefc96e1842
taxelrod <taxelrod@git.lsstcorp.org>
2007-03-14 20:24:23 +0000

    Added test of nested properties

--- tests/DataProperty_1.cc
+++ tests/DataProperty_1.cc
@@ -4,16 +4,47 @@
 
 using namespace lsst;
 
+class Foo {
+     int gurp;
+     std::string murp;
+     double durp;
+};
+
 int main()
 {
      DataProperty root("root");
 
      DataProperty prop1("name1", std::string("value1"));
      DataProperty prop2("name2", 2);
+     
 
      root.addProperty(prop1);
      root.addProperty(prop2);
 
+     Foo foo1;
+     DataProperty prop3("name3", foo1);
+     root.addProperty(prop3);
+
      DataProperty *dpPtr = root.find("name2");
      dpPtr->print();
+     dpPtr = root.find("name1");
+     dpPtr->print();
+     dpPtr = root.find("name3");
+     dpPtr->print();
+
+     // Try nested property list
+     
+     DataProperty nested("nested");
+
+     DataProperty nprop1("name1n", std::string("value1"));
+     DataProperty nprop2("name2n", 2);
+     
+
+     nested.addProperty(nprop1);
+     nested.addProperty(nprop2);
+
+     root.addProperty(nested);
+
+     root.print();
+
 }     
