2b55c513382106eee156012322f57b9f633ff61c
taxelrod <taxelrod@git.lsstcorp.org>
2007-03-19 17:53:04 +0000

    Added test of ability to continue find() from last place found

--- tests/DataProperty_1.cc
+++ tests/DataProperty_1.cc
@@ -16,6 +16,7 @@ int main()
 
      DataProperty prop1("name1", std::string("value1"));
      DataProperty prop2("name2", 2);
+     DataProperty prop2a("name2", 4);
      
 
      root.addProperty(prop1);
@@ -25,8 +26,15 @@ int main()
      DataProperty prop3("name3", foo1);
      root.addProperty(prop3);
 
+     root.addProperty(prop2a);
+
      DataProperty *dpPtr = root.find("name2");
      dpPtr->print();
+
+     // check find without reset to beginning
+     dpPtr = root.find("name2", false);
+     dpPtr->print();
+
      dpPtr = root.find("name1");
      dpPtr->print();
      dpPtr = root.find("name3");
