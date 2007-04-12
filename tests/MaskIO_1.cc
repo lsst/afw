fdf2d04e731c7bcf3cd3a6cf3125b011133d10a3
taxelrod <taxelrod@git.lsstcorp.org>
2007-04-12 21:51:03 +0000

    Beginning of new tester for Mask IO functionality

---
+++ tests/MaskIO_1.cc
@@ -0,0 +1,31 @@
+// -*- lsst-c++ -*-
+#include "lsst/fw/Mask.h"
+#include "lsst/fw/LSSTFitsResource.h"
+
+#include <stdexcept>
+
+using namespace lsst;
+using boost::any_cast;
+
+
+/*
+ * Make this a subroutine so that locals go out of scope as part of test
+ * of memory management
+ */
+void test() {
+    LSSTFitsResource inFits("filename.fits");
+    DataProperty::DataPropertyPtrT metaDataPtr = inFits.getMetaData();
+    metaDataPtr->print();
+}
+
+int main(int argc, char *argv[]) {
+    try {
+        try {
+            test();
+        } catch (lsst::Exception &e) {
+            throw lsst::Exception(std::string("In handler\n") + e.what());
+        }
+    } catch (lsst::Exception &e) {
+        std::clog << e.what() << endl;
+    }
+}
