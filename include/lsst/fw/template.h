e6e629cf8af9c46a3bd6ec19e33e2e6b2e64896d
jbartels <jbartels>
2007-04-19 18:13:49 +0000

    Initial revision

---
+++ include/lsst/fw/template.h
@@ -0,0 +1,36 @@
+/**
+  * \class Template
+  *
+  * \ingroup fw
+  *
+  * \brief Template class role
+  *
+  * This class is meant as an example. It is not useful by itself
+  * rather its usefulness is only a function of how much it helps 
+  * the reader. It is is a sense defined by the person who reads it
+  * and otherwise does not exist in any real form.
+  * 
+  * \note Attempts at zen rarely work
+  * 
+  * \author  $Author::                                            $
+  * \version $Rev::                                               $
+  * \date    $LastChangeDate::                                    $
+  * 
+  * $Id::                                                         $
+  * $HeadURL::                                                    $
+  * 
+  * Contact:
+  * 
+  * Created:
+  * 
+   
+  */
+ 
+/**
+  * \brief A useful method
+  * \param parmname parameter desription
+  * \return return value(s) description(s)
+  *
+  * This is a method description
+  *
+  */
