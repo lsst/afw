2e17ef06854acd1aaf0e0f0ae83e19acf4540688
rhl <rhl@git.lsstcorp.org>
2007-06-11 13:42:23 +0000

    Convert to unittest

--- tests/MaskedImage_1.py
+++ tests/MaskedImage_1.py
@@ -1,22 +1,71 @@
-import lsst.fw.Core.fwLib as fwCore
+"""
+Tests for Peaks, Footprints, and DetectionSets
+
+Run with:
+   python Footprint_1.py
+or
+   python
+   >>> import unittest; T=load("Footprint_1"); unittest.TextTestRunner(verbosity=1).run(T.suite())
+"""
+
+import pdb                              # we may want to say pdb.set_trace()
+import unittest
+import lsst.fw.Core.tests as tests
+import lsst.fw.Core.fwLib as fw
 import fwTests
 
-def test():
-    testMaskedImage1 = fwCore.MaskedImageD(272, 1037)
-    testMaskedImage2 = fwCore.MaskedImageD(272, 1037)
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
-    for tm in (testMaskedImage1, testMaskedImage2):
-        tm.getMask().addMaskPlane("CR")
+class MaskedImageTestCase(unittest.TestCase):
+    """A test case for MaskedImage"""
+    def setUp(self):
+        self.maskedImage1 = fw.MaskedImageD(272, 1037)
+        self.maskedImage2 = fw.MaskedImageD(272, 1037)
 
-    testMaskedImage2 += testMaskedImage1;
+        for m in (self.maskedImage1, self.maskedImage2):
+            m.getMask().addMaskPlane("CR")
 
-    fooFunc = fwTests.testPixProcFuncD(testMaskedImage1)
+    def tearDown(self):
+        del self.maskedImage1
+        del self.maskedImage2
 
-    fooFunc.init()
-    testMaskedImage1.processPixels(fooFunc)
+    def testGC(self):
+        """Check that Peaks are automatically garbage collected (when MemoryTestCase runs)"""
+        pass
 
-    fooFunc.init()
-    testMaskedImage1.processPixels(fooFunc)
+    def testAddMaskPlane(self):
+        """Check if we can add mask planes"""
+        mask = self.maskedImage1.getMask()
+
+        self.assertRaises(IndexError, mask.getMaskPlane, "XXX")
+
+        name = "FOO"
+        mask.addMaskPlane(name)
+        try:
+            mask.getMaskPlane(name)
+        except IndexError:
+            self.fail("Failed to add maskPlane %s" % name)
+
+    def testAddMaskedImages(self):
+        self.maskedImage2 += self.maskedImage1
+
+    def testPixelProc(self):
+        fooFunc = fwTests.testPixProcFuncD(self.maskedImage1)
+
+        fooFunc.init()
+        self.maskedImage1.processPixels(fooFunc)
+            
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+def suite():
+    """Returns a suite containing all the test cases in this module."""
+
+    tests.init()
+
+    suites = []
+    suites += unittest.makeSuite(MaskedImageTestCase)
+    suites += unittest.makeSuite(tests.MemoryTestCase)
+    return unittest.TestSuite(suites)
 
 if __name__ == "__main__":
-    test()
+    unittest.main()
