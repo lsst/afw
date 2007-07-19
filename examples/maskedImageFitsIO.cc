5ec55fee86d14901ff78b1b02efd8497ecdc7a49
rowen <rowen@git.lsstcorp.org>
2007-07-19 22:08:05 +0000

    Added example maskedImageFitsIO.cc which shows the problem in ticket 93

---
+++ examples/maskedImageFitsIO.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <sstream>
+
+#include <lsst/fw/MaskedImage.h>
+
+const std::string outFile("rwfitsOut");
+
+int main(int argc, char **argv) {
+    typedef float pixelType;
+    typedef unsigned int maskType;
+
+    lsst::fw::MaskedImage<pixelType, maskType> mImage;
+
+    if (argc < 2) {
+        std::cerr << "Usage: maskedImageFitsIO fitsFile" << std::endl;
+        std::cerr << "fitsFile excludes the \"_img.fits\" suffix" << std::endl;
+        return 1;
+    }
+
+    mImage.readFits(argv[1]);
+    mImage.writeFits(outFile);
+}
