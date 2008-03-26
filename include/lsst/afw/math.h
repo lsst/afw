33e4a0d8ac27ae9ad4317d6b19ed985487f2d2c5
rowen <rowen@git.lsstcorp.org>
2008-03-26 23:09:02 +0000

    afw reorg: - Rearranged src/ (except MovingObject...) - Added math.h include file - Tweaked a few files

---
+++ include/lsst/afw/math.h
@@ -0,0 +1,11 @@
+/**
+ * This include file includes the header files from the math directory
+ */
+#ifndef LSST_AFW_MATH_H
+#define LSST_AFW_MATH_H
+
+#include "lsst/afw/math/Kernel.h"
+#include "lsst/afw/math/minimize.h"
+#include "lsst/afw/math/FunctionLibrary.h"
+
+#endif // LSST_AFW_MATH_H
