c15438f75051d5456dca7ca25c3053afe0664d12
rhl <rhl@git.lsstcorp.org>
2008-04-08 13:55:05 +0000

    Use unittest

--- tests/MaskedImagePersistence_1.py
+++ tests/MaskedImagePersistence_1.py
@@ -1,55 +1,97 @@
 #!/usr/bin/env python
 import os
+import pdb                          # we may want to say pdb.set_trace()
+import unittest
+import lsst.utils.tests as utilsTests
 
 import lsst.afw.image as afwImage
 import lsst.daf.base as dafBase
 import lsst.daf.persistence as dafPers
-import lsst.pex.policy as dafPolicy
+import lsst.pex.policy as pexPolicy
+import lsst.pex.exceptions as pexExceptions
+import eups
 
-# Create the additionalData DataProperty
-additionalData = dafBase.DataProperty.createPropertyNode("root")
-additionalData.addProperty(dafBase.DataProperty("sliceId", 0))
-additionalData.addProperty(dafBase.DataProperty("visitId", "fov391"))
-additionalData.addProperty(dafBase.DataProperty("universeSize", 100))
-additionalData.addProperty(dafBase.DataProperty("itemName", "foo"))
+dataDir = eups.productDir("afwdata")
+if not dataDir:
+    raise RuntimeError("You must set up afwdata to run these tests")
 
-# Create an empty Policy
-policy = dafPolicy.PolicyPtr()
+class MaskedImagePersistenceTestCase(unittest.TestCase):
+    """A test case for MaskedImage Persistence"""
 
-# Get a Persistence object
-persistence = dafPers.Persistence.getPersistence(policy)
+    def checkImages(self, maskedImage, maskedImage2):
+        # Check that two MaskedImages are identical (well, actually check only every 4th pixel)
+        pass
 
-# Set up the LogicalLocation.  Assumes that previous tests have run, and
-# Src_*.fits exists in the current directory.
-logicalLocation = dafPers.LogicalLocation("Src")
+    def setUp(self):
+        # Create the additionalData DataProperty
+        self.additionalData = dafBase.DataProperty.createPropertyNode("root")
+        self.additionalData.addProperty(dafBase.DataProperty("sliceId", 0))
+        self.additionalData.addProperty(dafBase.DataProperty("visitId", "fov391"))
+        self.additionalData.addProperty(dafBase.DataProperty("universeSize", 100))
+        self.additionalData.addProperty(dafBase.DataProperty("itemName", "foo"))
 
-# Create a FitsStorage and put it in a StorageList.
-storage = persistence.getRetrieveStorage("FitsStorage", logicalLocation)
-storageList = dafPers.StorageList([storage])
+        # Create an empty Policy
+        policy = pexPolicy.PolicyPtr()
 
-print "Retrieving MaskedImage Src"
+        # Get a Persistence object
+        self.persistence = dafPers.Persistence.getPersistence(policy)
 
-# Let's do the retrieval!
-maskedImage = afwImage.MaskedImageF.swigConvert( \
-    persistence.unsafeRetrieve("MaskedImageF", storageList, additionalData))
+        # Choose a file to manipulate
+        self.infile = os.path.join(dataDir, "small_MI")
 
-# Check the resulting MaskedImage
-# ...
+        self.maskedImage = afwImage.MaskedImageF(); self.maskedImage.readFits(self.infile)
+        
+    def tearDown(self):
+        del self.additionalData
+        del self.persistence
+        del self.infile
+        del self.maskedImage
 
-print "Persisting MaskedImage as FITS to Dest"
+    def testFitsPersistence(self):
+        """Test persisting to FITS"""
 
-# Persist the MaskedImage (under a different name)
-logicalLocation = dafPers.LogicalLocation("Dest")
-storage = persistence.getPersistStorage("FitsStorage", logicalLocation)
-storageList = dafPers.StorageList([storage])
-try:
-    persistence.persist(maskedImage, storageList, additionalData)
-except dafPolicy.LsstInvalidParameter, e:
-    print e.what()
-    raise
+        # Set up the LogicalLocation.
+        logicalLocation = dafPers.LogicalLocation(self.infile)
 
-# Ideally should do a cmp to make sure they are the same, but the persistence
-# writes additional comments not present in the original file.
-# assert os.system("cmp Src_img.fits Dest_img.fits") == 0
-# assert os.system("cmp Src_msk.fits Dest_msk.fits") == 0
-# assert os.system("cmp Src_var.fits Dest_var.fits") == 0
+        # Create a FitsStorage and put it in a StorageList.
+        storage = self.persistence.getRetrieveStorage("FitsStorage", logicalLocation)
+        storageList = dafPers.StorageList([storage])
+
+        # Let's do the retrieval!
+        maskedImage2 = afwImage.MaskedImageF.swigConvert( \
+            self.persistence.unsafeRetrieve("MaskedImageF", storageList, self.additionalData))
+
+        # Check the resulting MaskedImage
+        self.checkImages(self.maskedImage, maskedImage2)
+
+    def testBoostPersistence(self):
+        """Persist the MaskedImage using boost"""
+
+        logicalLocation = dafPers.LogicalLocation("Dest")
+        storage = self.persistence.getPersistStorage("FitsStorage", logicalLocation)
+        storageList = dafPers.StorageList([storage])
+        try:
+            self.persistence.persist(self.maskedImage, storageList, self.additionalData)
+        except pexExceptions.LsstInvalidParameter, e:
+            print e.what()
+            raise
+
+
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+def suite():
+    """Returns a suite containing all the test cases in this module."""
+    utilsTests.init()
+
+    suites = []
+    suites += unittest.makeSuite(MaskedImagePersistenceTestCase)
+    suites += unittest.makeSuite(utilsTests.MemoryTestCase)
+
+    return unittest.TestSuite(suites)
+
+def run(exit=False):
+    """Run the tests"""
+    utilsTests.run(suite(), exit)
+
+if __name__ == "__main__":
+    run(True)
