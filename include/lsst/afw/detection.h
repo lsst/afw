8d159a78b7b7d32f6632fa8a3f037f939cf392b6
rhl <rhl@git.lsstcorp.org>
2008-11-25 18:33:20 +0000

    Don't include non-existent files

--- include/lsst/afw/detection.h
+++ include/lsst/afw/detection.h
@@ -5,8 +5,8 @@
 #ifndef LSST_AFW_DETECTION_H
 #define LSST_AFW_DETECTION_H
 
-#include "lsst/afw/detection/DetectionSet.h"
 #include "lsst/afw/detection/Footprint.h"
+#include "lsst/afw/detection/Peak.h"
 #include "lsst/afw/detection/Source.h"
 
 #endif
