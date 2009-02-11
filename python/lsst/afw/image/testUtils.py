e961df9fc95a9e3f95f306ec580b2d4a145db934
rowen <rowen@git.lsstcorp.org>
2009-02-11 20:32:13 +0000

    python/lsst/afw/image/testUtils.py improvements: - Finished converting testUtils to use afw standard order for MaskedImage components   (image, mask, variance instead of image, variance, mask). - Added support for MaskedImages with missing components.

--- python/lsst/afw/image/testUtils.py
+++ python/lsst/afw/image/testUtils.py
@@ -13,26 +13,33 @@ import lsst.afw.image as afwImage
 
 def arrayFromImage(im, dtype=float):
     """Return a numpy array representation of an image.
+    Return None if mask is None.
     The data is presently copied but do not rely on that.
     """
+    if im == None:
+        return None
     arr = numpy.zeros([im.getWidth(), im.getHeight()], dtype=dtype)
     for row in range(im.getHeight()):
         for col in range(im.getWidth()):
             arr[col, row] = im.get(col, row)
     return arr
 
-def arrayFromMask(im, dtype=int):
+def arrayFromMask(mask, dtype=int):
     """Return a numpy array representation of a mask.
+    Return None if mask is None.
     The data is presently copied but do not rely on that.
     """
-    arr = numpy.zeros([im.getWidth(), im.getHeight()], dtype=dtype)
-    for row in range(im.getHeight()):
-        for col in range(im.getWidth()):
-            arr[col, row] = im.get(col, row)
+    if mask == None:
+        return None
+    arr = numpy.zeros([mask.getWidth(), mask.getHeight()], dtype=dtype)
+    for row in range(mask.getHeight()):
+        for col in range(mask.getWidth()):
+            arr[col, row] = mask.get(col, row)
     return arr
 
 def arraysFromMaskedImage(maskedImage):
     """Return a tuple of (image, mask, variance) arrays from a MaskedImage.
+    Return None for any missing component.
     The data is presently copied but do not rely on that.
     """
     return (
@@ -63,13 +70,13 @@ def maskFromArray(arr):
     setMaskFromArray(mask, arr)
     return mask
 
-def maskedImageFromArrays(imVarMaskArrays, retType=afwImage.MaskedImageF):
-    """Return a MaskedImage representation of a tuple of (image, variance, mask) numpy arrays.
+def maskedImageFromArrays(imMaskVarArrays, retType=afwImage.MaskedImageF):
+    """Return a MaskedImage representation of a tuple of (image, mask, variance) numpy arrays.
     The data is presently copied but do not rely on that.
     """
-    imArr = imVarMaskArrays[0]
+    imArr = imMaskVarArrays[0]
     maskedImage = retType(imArr.shape[0], imArr.shape[1])
-    setMaskedImageFromArrays(maskedImage, imVarMaskArrays)
+    setMaskedImageFromArrays(maskedImage, imMaskVarArrays)
     return maskedImage
 
 def setImageFromArray(im, arr):
@@ -92,11 +99,12 @@ def setMaskFromArray(mask, arr):
         for col in range(mask.getWidth()):
             mask.set(col, row, int(arr[col, row]))
 
-def setMaskedImageFromArrays(maskedImage, imVarMaskArrays):
-    """Set an existing lsst.afwImage.MaskedImage (of any type) from a numpy array.
+def setMaskedImageFromArrays(maskedImage, imMaskVarArrays):
+    """Set an existing lsst.afwImage.MaskedImage (of any type) from a of a tuple of (image, mask, variance) numpy arrays.
+    If image or variance arrays are None then that component is not set.
     The data is presently copied but do not rely on that.
     """
-    imArr, varArr, maskArr = imVarMaskArrays
+    imArr, maskArr, varArr = imMaskVarArrays
     if not (imArr.shape == varArr.shape == maskArr.shape):
         raise RuntimeError("The arrays must all be the same shape")
     if not (maskedImage.getWidth() == imArr.shape[0] and maskedImage.getHeight() == imArr.shape[1]):
@@ -106,8 +114,10 @@ def setMaskedImageFromArrays(maskedImage, imVarMaskArrays):
     for row in range(maskedImage.getHeight()):
         for col in range(maskedImage.getWidth()):
             im.set(col, row, imArr[col, row])
-            mask.set(col, row, maskArr[col, row])
-            var.set(col, row, varArr[col, row])
+            if mask:
+                mask.set(col, row, maskArr[col, row])
+            if var:
+                var.set(col, row, varArr[col, row])
 
 if __name__ == "__main__":
     maskedImage = afwImage.MaskedImageD("data/small")
