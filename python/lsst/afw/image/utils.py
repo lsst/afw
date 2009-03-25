33afdf2a453d4892d215d5d9f459a6a3ea3f7a2c
rhl <rhl@git.lsstcorp.org>
2009-03-25 19:14:51 +0000

    Added utility to clip an image to a range of values

---
+++ python/lsst/afw/image/utils.py
@@ -0,0 +1,19 @@
+import re
+import lsst.afw.detection as afwDetection
+import lsst.afw.image as afwImage
+
+def clipImage(im, min, max):
+    """Clip an image to lie between min and max (None to ignore)"""
+
+    if re.search("::MaskedImage<", im.__repr__()):
+        mi = im
+    else:
+        mi = afwImage.makeMaskedImage(im, afwImage.MaskU(im.getDimensions()))
+
+    if min is not None:
+        ds = afwDetection.makeDetectionSet(mi, afwDetection.Threshold(-min, afwDetection.Threshold.VALUE, False))
+        afwDetection.setImageFromFootprintList(mi.getImage(), ds.getFootprints(), min)
+
+    if max is not None:
+        ds = afwDetection.makeDetectionSet(mi, afwDetection.Threshold(max))
+        afwDetection.setImageFromFootprintList(mi.getImage(), ds.getFootprints(), max)
