92e48dacd779245d9fba501c0ec2140aadd974ce
rhl <rhl@git.lsstcorp.org>
2010-06-07 21:11:32 +0000

    Set HeadURL expansion correctly command

--- python/lsst/afw/__init__.py
+++ python/lsst/afw/__init__.py
@@ -3,6 +3,6 @@ import lsst.utils
 def version():
     """Return current version. If a different version is setup, return that too"""
 
-    HeadURL = r"$HeadURL: svn+ssh://svn.lsstcorp.org/DMS/afw/tags/3.5.2/python/lsst/afw/__init__.py $"
+    HeadURL = r"$HeadURL$"
     return lsst.utils.version(HeadURL, "afw")
 
