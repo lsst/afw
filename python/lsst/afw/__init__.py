2c7547dd602fe4b58b3ebcf8d3e36933f5ae5893
rhl <rhl@git.lsstcorp.org>
2010-06-07 21:04:58 +0000

    Add version() command

--- python/lsst/afw/__init__.py
+++ python/lsst/afw/__init__.py
@@ -1,1 +1,8 @@
+import lsst.utils
+
+def version():
+    """Return current version. If a different version is setup, return that too"""
+
+    HeadURL = r"$HeadURL: svn+ssh://svn.lsstcorp.org/DMS/afw/tags/3.5.2/python/lsst/afw/__init__.py $"
+    return lsst.utils.version(HeadURL, "afw")
 
