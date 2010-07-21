24a69fe0669bf5346bfcce72fa2715a85ddf43db
dstn <dstn@git.lsstcorp.org>
2010-07-21 20:02:34 +0000

    modify testWcs-1.wcs to make it readable by awf.image

--- tests/testWcs.py
+++ tests/testWcs.py
@@ -1,4 +1,5 @@
 import os.path
+from math import *
 
 import eups
 import lsst.afw.image as afwImg
@@ -6,7 +7,9 @@ import lsst.afw.geom as afwGeom
 
 mypath = eups.productDir('afw')
 
-fn = os.path.join(mypath, 'tests', 'testWcs-1.wcs')
+# fails due to #1386
+#fn = os.path.join(mypath, 'tests', 'testWcs-1.wcs')
+fn = os.path.join(mypath, 'tests', 'testWcs-2.wcs')
 fitsheader = afwImg.readMetadata(fn)
 wcs = afwImg.makeWcs(fitsheader)
 
@@ -15,6 +18,12 @@ print 'pixel area:', pa
 print 'sqrt', sqrt(pa)
 print '3600 sqrt', 3600. * sqrt(pa)
 
+pa = wcs.pixArea(afwGeom.makePointD(359.9823936, -3.9997742));
+print 'pixel area:', pa
+print 'sqrt:', sqrt(pa)
+print '3600 x sqrt:', 3600. * sqrt(pa)
 
 
     
+
+
