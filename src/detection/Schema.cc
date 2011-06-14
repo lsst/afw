932f613bf8c460f7dae98fe6727089bd6744fcb0
ktlim <ktlim@git.lsstcorp.org>
2011-06-14 23:13:38 +0000

    Add void case if blob data is never set.  Add archives back into schema since it turns out export does *not* automatically include them. (Merge [22452] from trunk)

--- src/detection/Schema.cc
+++ src/detection/Schema.cc
@@ -3,6 +3,12 @@
 namespace afwDetect = lsst::afw::detection;
 
 #include <boost/serialization/export.hpp>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/binary_oarchive.hpp>
+#include <boost/archive/binary_iarchive.hpp>
+#include <boost/archive/xml_oarchive.hpp>
+#include <boost/archive/xml_iarchive.hpp>
 
 BOOST_CLASS_EXPORT(afwDetect::SchemaEntry)
 
