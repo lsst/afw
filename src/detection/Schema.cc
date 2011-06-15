ceb8d60d43acaf71a8d0ac8024ed208c8f3a85da
ktlim <ktlim@git.lsstcorp.org>
2011-06-15 04:59:42 +0000

    Make sure archives are included before exports so that proper instantiations and registrations are performed.  Solves SEGV issue with pickling.  (Merge [22459] from trunk.)

--- src/detection/Schema.cc
+++ src/detection/Schema.cc
@@ -2,13 +2,13 @@
 
 namespace afwDetect = lsst::afw::detection;
 
-#include <boost/serialization/export.hpp>
 #include <boost/archive/text_oarchive.hpp>
 #include <boost/archive/text_iarchive.hpp>
 #include <boost/archive/binary_oarchive.hpp>
 #include <boost/archive/binary_iarchive.hpp>
 #include <boost/archive/xml_oarchive.hpp>
 #include <boost/archive/xml_iarchive.hpp>
+#include <boost/serialization/export.hpp>
 
 BOOST_CLASS_EXPORT(afwDetect::SchemaEntry)
 
