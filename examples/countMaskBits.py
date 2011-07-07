25ac393483dbf99a65faf8e975d4449e7912e22d
rowen <rowen@git.lsstcorp.org>
2011-07-07 22:13:45 +0000

    Added # of non-finite pixels to examples/countMaskBits.py

--- examples/countMaskBits.py
+++ examples/countMaskBits.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-"""Report how many of each bit plane is set in the mask
+"""Report how many of each bit plane is set in the mask and the number of non-finite pixels.
 
 Usage:
 ./countMaskBits.py path-to-masked-image
@@ -46,6 +46,29 @@ def countInterp(maskedImage):
     numInterp = numpy.sum(isInterpArr)
     numBadAndInterp = numpy.sum(isBadArr & isInterpArr)
     return numBad, numInterp, numBadAndInterp
+    
+def countNotFinite(maskedImage):
+    """Count non-finite pixels (NaNs, infs, etc.), and how many of those are non-EDGE pixels
+    
+    @param[in] maskedImage: an afwImage MaskedImage
+
+    @return
+    - numImNotFinite: number of image pixels that are not finite
+    - numVarNotFinite: number of variance pixels that are not finite
+    - numImNotEdgeOrFinite: number of image pixels that are not edge or finite
+    - numVarNotEdgeOrFinite: number of variance pixels that are not edge or finite
+    """
+    imArr = maskedImage.getImage().getArray()
+    varArr = maskedImage.getVariance().getArray()
+    maskArr = maskedImage.getMask().getArray()
+    numImNotFinite = numpy.sum(numpy.logical_not(numpy.isfinite(imArr)))
+    numVarNotFinite = numpy.sum(numpy.logical_not(numpy.isfinite(varArr)))
+    edgeMask = afwImage.MaskU.getPlaneBitMask("EDGE")
+    isEdge = maskArr & edgeMask
+    numImNotEdgeOrFinite = numpy.sum(numpy.logical_not(numpy.logical_or(numpy.isfinite(imArr), isEdge)))
+    numVarNotEdgeOrFinite = numpy.sum(numpy.logical_not(numpy.logical_or(numpy.isfinite(varArr), isEdge)))
+    return numImNotFinite, numVarNotFinite, numImNotEdgeOrFinite, numVarNotEdgeOrFinite
+    
 
 if __name__ == "__main__":
     maskedImage = afwImage.MaskedImageF(sys.argv[1])
@@ -66,3 +89,9 @@ if __name__ == "__main__":
     numBad, numInterp, numBadAndInterp = countInterp(maskedImage)
     print "%d bad; %d interp; %d bad & interp; %d bad and not interp; %d good but interp" % \
         (numBad, numInterp, numBadAndInterp, numBad - numBadAndInterp, numInterp - numBadAndInterp)
+
+    print
+    numImNotFinite, numVarNotFinite, numImNotEdgeOrFinite, numVarNotEdgeOrFinite = countNotFinite(maskedImage)
+    print "%d non-finite image pixels; of these %d are not EDGE" % (numImNotFinite, numImNotEdgeOrFinite)
+    print "%d non-finite variance pixels; of these %d are not EDGE" % (numVarNotFinite, numVarNotEdgeOrFinite)
+    
