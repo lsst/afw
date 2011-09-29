43549001a7442d37ac1b32fe0c6178ce023d2387
rhl <rhl@git.lsstcorp.org>
2011-09-29 05:00:47 +0000

    Improved comment

--- python/lsst/afw/display/ds9.py
+++ python/lsst/afw/display/ds9.py
@@ -226,25 +226,26 @@ except NameError:
 annoying but necessary for anything resembling performance
 
 The usual usage pattern (from a module importing this file, ds9.py) is probably:
-   ds9.cmdBuffer.pushSize()
-   # bunches of ds9.{dot,line} commands
-   ds9.cmdBuffer.flush()
-   # bunches more ds9.{dot,line} commands
-   ds9.cmdBuffer.popSize()
 
-N.b. These are available as:
+   with ds9.Buffering():
+       # bunches of ds9.{dot,line} commands
+       ds9.flush()
+       # bunches more ds9.{dot,line} commands
+
+or (if you don't like "with")
    ds9.buffer()
    # bunches of ds9.{dot,line} commands
    ds9.flush()
    # bunches more ds9.{dot,line} commands
    ds9.buffer(False)
 
-or (better)
-   with ds9.Buffering():
-       # bunches of ds9.{dot,line} commands
-       ds9.flush()
-       # bunches more ds9.{dot,line} commands
+or (the old idiom):
    
+   ds9.cmdBuffer.pushSize()
+   # bunches of ds9.{dot,line} commands
+   ds9.cmdBuffer.flush()
+   # bunches more ds9.{dot,line} commands
+   ds9.cmdBuffer.popSize()
         """
 
         def __init__(self, size=0):
