bf2e526d0d513b51065648bf7c3816a35e83b9e3
Jim Bosch <jbosch@astro.princeton.edu>
2011-12-02 14:55:18 -0500

    removed asIterator member functions from records

--- include/lsst/afw/table/RecordInterface.h
+++ include/lsst/afw/table/RecordInterface.h
@@ -51,8 +51,6 @@ class RecordInterface : public detail::RecordBase {
 public:
 
     typedef typename Tag::Record Record;
-    typedef boost::transform_iterator<detail::RecordConverter<Record>,detail::TreeIteratorBase> TreeIterator;
-    typedef boost::transform_iterator<detail::RecordConverter<Record>,detail::IteratorBase> Iterator;
     typedef ChildrenView<Tag> Children;
 
     Record getParent() const {
@@ -61,14 +59,6 @@ public:
 
     Children getChildren(TreeMode mode) const {
         return Children(*this, mode);
-    }
-
-    TreeIterator asTreeIterator(TreeMode mode) const {
-        return TreeIterator(this->_asTreeIterator(mode), detail::RecordConverter<Record>());
-    }
-
-    Iterator asIterator() const {
-        return Iterator(this->_asIterator(), detail::RecordConverter<Record>());
     }
 
 protected:
--- include/lsst/afw/table/detail/RecordBase.h
+++ include/lsst/afw/table/detail/RecordBase.h
@@ -79,9 +79,6 @@ protected:
         return RecordBase(_data->links.parent, _table, *this);
     }
 
-    TreeIteratorBase _asTreeIterator(TreeMode mode) const;
-    IteratorBase _asIterator() const;
-
     TreeIteratorBase _beginChildren(TreeMode mode) const;
     TreeIteratorBase _endChildren(TreeMode mode) const;
 
