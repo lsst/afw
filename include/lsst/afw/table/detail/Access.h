9a205e3c73d9b0a8471ec0cd71f8e9656bd5f39d
Jim Bosch <jbosch@astro.princeton.edu>
2012-02-03 22:53:58 -0500

    working on internals documentation

--- include/lsst/afw/table/detail/Access.h
+++ include/lsst/afw/table/detail/Access.h
@@ -16,9 +16,20 @@ class BaseTable;
 
 namespace detail {
 
+/**
+ *  @internal
+ *
+ *  @brief Friendship-aggregation class for afw/table.
+ *
+ *  Access is a collection of static member functions that provide access to internals of other
+ *  classes.  It allows many classes to just declare Access as a friend rather than a long list of
+ *  related classes.  This is less secure, but it's obviously not part of the public interface,
+ *  and that's good enough.
+ */
 class Access {
 public:
 
+    /// @internal @brief Return a sub-field key corresponding to the nth element.
     template <typename T>
     static Key<typename Key<T>::Element> extractElement(KeyBase<T> const & kb, int n) {
         return Key<typename Key<T>::Element>(
@@ -26,26 +37,24 @@ public:
         );
     }
 
+    /// @internal @brief Access to the private Key constructor.
     template <typename T>
     static Key<T> makeKey(Field<T> const & field, int offset) {
         return Key<T>(offset, field);
     }
 
+    /// @internal @brief Access to the private Key constructor.
     static Key<Flag> makeKey(int offset, int bit) {
         return Key<Flag>(offset, bit);
     }
 
+    /// @internal @brief Add some padding to a schema without adding a field.
     static void padSchema(Schema & schema, int bytes) {
         schema._edit();
         schema._impl->_recordSize += bytes;
     }
 
-    template <typename ContainerT>
-    static void readFits(std::string const & filename, ContainerT & container);
-
-    template <typename ContainerT>
-    static void writeFits(std::string const & filename, ContainerT const & container);
-
+    /// @internal @brief Mark a schema as persistent (SchemaImpl is what inherits from Citizen).
     static void markPersistent(Schema const & schema) {
         schema._impl->markPersistent();
     }
