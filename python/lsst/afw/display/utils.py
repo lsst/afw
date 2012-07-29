11e01837b0f74d63efeb4ed3a0fe45a85ed6eff1
Robert Lupton the Good <rhl@astro.princeton.edu>
2012-07-29 15:45:52 +0900

    Minor changes to mosaic code.  If height>width 1x1 mosaic ended up as 1x2...

--- python/lsst/afw/display/utils.py
+++ python/lsst/afw/display/utils.py
@@ -79,7 +79,10 @@ class Mosaic(object):
         
     def append(self, image, label=None, ctype=None):
         """Add an image to the list of images to be mosaiced
-        Set may be cleared with Mosaic.reset()"""
+        Set may be cleared with Mosaic.reset()
+
+        Returns the index of this image (may be passed to getBBox())
+        """
         if not self.xsize:
             self.xsize = image.getWidth()
             self.ysize = image.getHeight()
@@ -87,7 +90,9 @@ class Mosaic(object):
         self.images.append(image)
         self.labels.append((label, ctype))
 
-    def makeMosaic(self, images=None, frame=None, mode=None, title=""):
+        return len(self.images)
+
+    def makeMosaic(self, images=None, frame=None, mode=None, background=None, title=""):
         """Return a mosaic of all the images provided; if none are specified,
         use the list accumulated with Mosaic.append()
         
@@ -109,7 +114,9 @@ class Mosaic(object):
             if h > self.ysize:
                 self.ysize = h
 
-        if not mode:
+        if background is None:
+            background = self.background
+        if mode is None:
             mode = self.mode
 
         if mode == "square":
@@ -122,7 +129,10 @@ class Mosaic(object):
                     ny += 1
                 if nx*ny < self.nImage:
                     nx += 1
-                    
+
+            if nx > self.nImage:
+                nx = self.nImage
+                
             assert(nx*ny >= self.nImage)
         elif mode == "x":
             nx, ny = self.nImage, 1
