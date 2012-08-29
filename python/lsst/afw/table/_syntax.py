90e7693682b5aa1f5863e08ead2a108ddfc87527
Jim Bosch <jbosch@astro.princeton.edu>
2012-08-29 15:41:55 -0400

    tweak docstrings, add LSST license boilerplate

--- python/lsst/afw/table/_syntax.py
+++ python/lsst/afw/table/_syntax.py
@@ -1,3 +1,24 @@
+# 
+# LSST Data Management System
+# Copyright 2008, 2009, 2010, 2011, 2012 LSST Corporation.
+# 
+# This product includes software developed by the
+# LSST Project (http://www.lsst.org/).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the LSST License Statement and 
+# the GNU General Public License along with this program.  If not, 
+# see <http://www.lsstcorp.org/LegalNotices/>.
+#
 """
 Special Python syntactic sugar for Catalogs and Records.
 
@@ -41,7 +62,7 @@ def Schema_extract(self, *patterns, **kwds):
                    as positional arguments.  Note that this will be compared with re.match,
                    not re.search.
 
-      sub -------- A replacement string (see re.MatchObject.expand) used to set the
+      sub -------- A replacement string template (see re.MatchObject.expand) used to set the
                    dictionary keys of any fields matched by regex.  The field name in the
                    SchemaItem is not modified.
 
