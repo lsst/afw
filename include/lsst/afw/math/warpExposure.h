707b1ed637c3e70694ea4de66a9cc50e164a14a8
Robert Lupton the Good <rhl@astro.princeton.edu>
2012-10-17 11:06:50 -0500

    Added swig pragmas as per #2392

--- include/lsst/afw/math/warpExposure.h
+++ include/lsst/afw/math/warpExposure.h
@@ -108,6 +108,9 @@ namespace math {
          * (which is why it defined in the BilinearWarpingKernel class instead of
          * being made available as a standalone function).
          */
+#if defined(SWIG)
+    #pragma SWIG nowarn=SWIGWARN_PARSE_NESTED_CLASS
+#endif
         class BilinearFunction1: public Function1<Kernel::Pixel> {
         public:
             typedef PTR(Function1<Kernel::Pixel>) Function1Ptr;
@@ -160,6 +163,9 @@ namespace math {
          * (which is why it defined in the NearestWarpingKernel class instead of
          * being made available as a standalone function).
          */
+#if defined(SWIG)
+    #pragma SWIG nowarn=SWIGWARN_PARSE_NESTED_CLASS
+#endif
         class NearestFunction1: public Function1<Kernel::Pixel> {
         public:
             typedef PTR(Function1<Kernel::Pixel>) Function1Ptr;
