588fbc3d1cf8a76158c0c5f6003d49b1b9bf284e
Jim Bosch <jbosch@astro.princeton.edu>
2012-11-02 17:25:34 -0400

    Add PointIterator class and begin()/end()/operator<< to Span.

--- include/lsst/afw/geom/Span.h
+++ include/lsst/afw/geom/Span.h
@@ -25,12 +25,14 @@
 #define LSST_AFW_GEOM_Span_h_INCLUDED
 
 #include <string>
+#include <iostream>
 
 #include "boost/serialization/nvp.hpp"
 
 #include "lsst/base.h"
 #include "lsst/afw/geom/Point.h"
 #include "lsst/afw/geom/Extent.h"
+#include "lsst/afw/geom/PointIterator.h"
 
 namespace boost{
 namespace serialization{
@@ -55,12 +57,22 @@ public:
     typedef boost::shared_ptr<Span> Ptr;
     typedef boost::shared_ptr<Span const> ConstPtr;
 
+    /// An iterator over points in the Span.
+    typedef PointIterator Iterator;
+
     Span(int y,                         //!< Row that Span's in
          int x0,                        //!< Starting column (inclusive)
          int x1)                        //!< Ending column (inclusive)
         : _y(y), _x0(x0), _x1(x1) {}
 
+    /// Construct an empty Span with zero width at the origin.
     Span() : _y(0), _x0(0), _x1(-1) {}
+
+    /// Return an iterator to the first pixel in the Span.
+    Iterator begin() const { return Iterator(Point2I(_x0, _y)); }
+
+    /// Return an iterator to one past the last pixel in the Span.
+    Iterator end() const { return Iterator(Point2I(_x1+1, _y)); }
 
     int getX0() const { return _x0; }         ///< Return the starting x-value
     int getX1() const { return _x1; }         ///< Return the ending x-value
@@ -72,19 +84,21 @@ public:
     int getEndX() const { return _x1+1; }       ///< End (exclusive) x-value.
     Point2I const getMin() const { return Point2I(_x0, _y); } ///< Point corresponding to minimum x.
     Point2I const getMax() const { return Point2I(_x1, _y); } ///< Point corresponding to maximum x.
-    Point2I const getBegin() const { return Point2I(_x0, _y); } ///< Box-like inclusive start point.
-    Point2I const getEnd() const { return Point2I(_x1+1, _y+1); } ///< Box-like exclusive end point.
 
     bool contains(int x) const { return (x >= _x0) && (x <= _x1); }
     bool contains(int x, int y) const { return (x >= _x0) && (x <= _x1) && (y == _y); }
     bool contains(Point2I const & point) const { return contains(point.getX(), point.getY()); }
 
+    /// Return true if the span contains no pixels.
     bool isEmpty() const { return _x1 < _x0; }
 
     /// Return a string-representation of a Span
     std::string toString() const;
 
     void shift(int dx, int dy) { _x0 += dx; _x1 += dx; _y += dy; }
+
+    /// Stream output; delegates to toString().
+    friend std::ostream & operator<<(std::ostream & os, Span const & span) { return os << span.toString(); }
 
     bool operator==(Span const & other) const {
         return other.getY() == getY() && other.getMinX() == getMinX() && other.getMaxX() == getMaxX();
