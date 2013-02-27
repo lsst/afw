4345903d6731bbc78498e485e1f8c8cf56b15b48
Jim Bosch <jbosch@astro.princeton.edu>
2013-02-27 22:16:13 +0900

    Workaround for FITS files that incorrectly put quotes around 'T' and 'F' when specifying INHERIT

--- src/image/Utils.cc
+++ src/image/Utils.cc
@@ -8,10 +8,18 @@ PTR(daf::base::PropertySet) readMetadata(std::string const& fileName, int hdu, b
     fits::Fits fitsfile(fileName, "r", fits::Fits::AUTO_CLOSE | fits::Fits::AUTO_CHECK);
     fitsfile.setHdu(hdu);
     fitsfile.readMetadata(*metadata, strip);
-    if (fitsfile.getHdu() != 1 && metadata->get<bool>("INHERIT", false)) {
-        if (strip) metadata->remove("INHERIT");
-        fitsfile.setHdu(1);
-        fitsfile.readMetadata(*metadata, strip);
+    if (fitsfile.getHdu() != 1 && metadata->exists("INHERIT")) {
+        bool inherit = false;
+        if (metadata->typeOf("INHERIT") == typeid(std::string)) {
+            inherit = (metadata->get<std::string>("INHERIT") == "T");
+        } else {
+            inherit = metadata->get<bool>("INHERIT");
+        }
+        if (inherit) {
+            if (strip) metadata->remove("INHERIT");
+            fitsfile.setHdu(1);
+            fitsfile.readMetadata(*metadata, strip);
+        }
     }
     return metadata;
 }
