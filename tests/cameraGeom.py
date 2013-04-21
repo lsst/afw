5aa76e92bd47325bce8d8863c7bd6851122d416d
Robert Lupton the Good <rhl@astro.princeton.edu>
2013-04-21 09:03:40 -0400

    Oops, I meant to leave the old test enabled (see #2831)

--- tests/cameraGeom.py
+++ tests/cameraGeom.py
@@ -124,7 +124,7 @@ class CameraGeomTestCase(unittest.TestCase):
 
     def assertImagesAreEqual(self, outImage, compImage):
         """Assert that two images have all pixels equal"""
-        if False:                        # Incorrect old test
+        if True:                        # Incorrect old test
             self.assertTrue(not (outImage.getArray() - compImage.getArray()).all())
         else:
             self.assertTrue((outImage.getArray() == compImage.getArray()).all())
