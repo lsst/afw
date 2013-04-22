6d4dbdcd18f8cc15f8075b220140ad87d17494ab
Jim Bosch <jbosch@astro.princeton.edu>
2013-04-22 17:30:01 -0400

    Doc improvements, Angle usage, and other minor fixes for transforms.

--- include/lsst/afw/geom/AffineTransform.h
+++ include/lsst/afw/geom/AffineTransform.h
@@ -1,33 +1,34 @@
 // -*- lsst-c++ -*-
-
-/* 
+/*
  * LSST Data Management System
- * Copyright 2008, 2009, 2010 LSST Corporation.
- * 
- * This product includes software developed by the
+ * Copyright 2008-2013 LSST Corporation.
+ *
+ * This product includes so@ftware developed by the
  * LSST Project (http://www.lsst.org/).
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
- * You should have received a copy of the LSST License Statement and 
- * the GNU General Public License along with this program.  If not, 
+ *
+ * You should have received a copy of the LSST License Statement and
+ * the GNU General Public License along with this program.  If not,
  * see <http://www.lsstcorp.org/LegalNotices/>.
  */
- 
-#ifndef LSST_AFW_MATH_AFFINE_TRANSFORM_H
-#define LSST_AFW_MATH_AFFINE_TRANSFORM_H
+
+#ifndef LSST_AFW_GEOM_AFFINE_TRANSFORM_H
+#define LSST_AFW_GEOM_AFFINE_TRANSFORM_H
 
 #include <boost/shared_ptr.hpp>
 #include "Eigen/Core"
 #include <iostream>
+
+#include "lsst/afw/geom/Angle.h"
 #include "lsst/afw/geom/Point.h"
 #include "lsst/afw/geom/Extent.h"
 #include "lsst/afw/geom/LinearTransform.h"
@@ -39,7 +40,7 @@ namespace geom {
 /**
  *  \brief An affine coordinate transformation consisting of a linear transformation and an offset.
  *
- *  The transform is represented by a matrix \f$ \mathbf{M} \f$ such that
+ *  The transform is represented by a matrix @f$ \mathbf{M} @f$ such that
  *  \f[
  *     \left[\begin{array}{ c }
  *     x_f \\
@@ -54,9 +55,9 @@ namespace geom {
  *     1
  *     \end{array}\right]
  *  \f]
- *  where \f$(x_i,y_i)\f$ are the input coordinates and \f$(x_f,y_f)\f$ are the output coordinates.
+ *  where @f$(x_i,y_i)@f$ are the input coordinates and @f$(x_f,y_f)@f$ are the output coordinates.
  *
- *  If \f$ x_f(x_i,y_i) \f$ and \f$ y_f(x_i,y_i) \f$ are continuous differentiable functions, then
+ *  If @f$ x_f(x_i,y_i) @f$ and @f$ y_f(x_i,y_i) @f$ are continuous differentiable functions, then
  *  \f[
  *     \mathbf{M} = \left[\begin{array}{ c c c }
  *     \displaystyle\frac{\partial x_f}{\partial x_i} &
@@ -68,101 +69,116 @@ namespace geom {
  *     \displaystyle 0 & \displaystyle 0 & \displaystyle 1
  *     \end{array}\right]
  *  \f]
- *  evaluated at \f$(x_i,y_i)\f$.
+ *  evaluated at @f$(x_i,y_i)@f$.
  *
- *  The 2x2 upper left corner of \f$ \mathbf{M} \f$ is the linear part of the transform is simply the
- *  Jacobian of the mapping between \f$(x_i,y_i)\f$ and \f$(x_f,y_f)\f$.
+ *  The 2x2 upper left corner of @f$ \mathbf{M} @f$ (the LinearTransform part) is simply the
+ *  Jacobian of the mapping between @f$(x_i,y_i)@f$ and @f$(x_f,y_f)@f$.
  */
 class AffineTransform {
 public:
-    typedef boost::shared_ptr<AffineTransform> Ptr;
-    typedef boost::shared_ptr<AffineTransform const> ConstPtr;
-
+    /// Enum to provide indices for matrix elements when we have to flatten them (e.g. for derivatives)
     enum Parameters {XX=0,YX=1,XY=2,YY=3,X=4,Y=5};
 
+    /// Matrix type used to define the transform itself.
     typedef Eigen::Matrix3d Matrix;
+
+    /// Type used when flattening matrix elements into a vector.
     typedef Eigen::Matrix<double,6,1> ParameterVector;
+
+    /// Type used when computing the derivative of a transformed point with respect to the transform.
     typedef Eigen::Matrix<double,2,6> TransformDerivativeMatrix;
 
-
-    /** Construct an empty (identity) AffineTransform. */
+    /// Construct an identity AffineTransform.
     AffineTransform() : _linear(), _translation() {}
 
-    /** Construct an AffineTransform from a 3x3 matrix. */
-    explicit AffineTransform(Eigen::Matrix3d const & matrix) 
-      : _linear(matrix.block<2,2>(0,0)), 
-        _translation(matrix.block<2,1>(0,2)) 
+    /// Construct an AffineTransform from a 3x3 matrix.
+    explicit AffineTransform(Eigen::Matrix3d const & matrix)
+      : _linear(matrix.block<2,2>(0,0)),
+        _translation(matrix.block<2,1>(0,2))
     {}
 
-    /** Construct an AffineTransform with no translation from a 2x2 matrix. */
-    explicit AffineTransform(Eigen::Matrix2d const & linear) 
+    /// Construct an AffineTransform with no translation from a 2x2 matrix.
+    explicit AffineTransform(Eigen::Matrix2d const & linear)
       : _linear(linear), _translation() {}
 
-    /** Construct a translation-only AffineTransform from a vector. */
-    explicit AffineTransform(Eigen::Vector2d const & translation) 
+    /// Construct a translation-only AffineTransform from a vector.
+    explicit AffineTransform(Eigen::Vector2d const & translation)
       : _linear(), _translation(translation) {}
 
-    /** Construct an AffineTransform from a 2x2 matrix and vector. */
+    /// Construct an AffineTransform from a 2x2 matrix and vector.
     explicit AffineTransform(
         Eigen::Matrix2d const & linear, Eigen::Vector2d const & translation
     ) : _linear(linear), _translation(translation) {}
-    
-    /** Construct an AffineTransform from a LinearTransform. */
-    explicit AffineTransform(LinearTransform const & linear) 
+
+    /// Construct an AffineTransform from a LinearTransform.
+    explicit AffineTransform(LinearTransform const & linear)
       : _linear(linear), _translation() {}
 
-    /** Construct a translation-only AffineTransform from an Extent2D. */
-    explicit AffineTransform(Extent2D const & translation) 
+    /// Construct a translation-only AffineTransform from an Extent2D.
+    explicit AffineTransform(Extent2D const & translation)
       : _linear(), _translation(translation) {}
 
-    /** Construct an AffineTransform from a LinearTransform and Extent2D. */
+    /// Construct an AffineTransform from a LinearTransform and Extent2D.
     explicit AffineTransform(
         LinearTransform const & linear, Extent2D const & translation
     ) : _linear(linear), _translation(translation) {}
 
-
+    /// Invert the transform (just matrix inversion).
     AffineTransform const invert() const;
 
-    /** Whether the transform is a no-op. */
+    /// Whether the transform is a no-op.
     bool isIdentity() const { return getMatrix().isIdentity(); }
 
-
-    /** 
-     * Transform a Point object. 
+    /**
+     * @brief Transform a Point object.
      *
      * The result is affected by the translation parameters of the transform
      */
-    Point2D operator()(Point2D const &p) const {         
+    Point2D operator()(Point2D const &p) const {
         return Point2D(_linear(p) + _translation);
     }
 
     /**
-     * Transform an Extent object. 
+     * @brief Transform an Extent object.
      *
      * The result is unaffected by the translation parameters of the transform
      */
-    Extent2D operator()(Extent2D const &p) const {         
+    Extent2D operator()(Extent2D const &p) const {
         return Extent2D(_linear(p));
     }
 
-    Extent2D const & getTranslation() const {return _translation;}
-    Extent2D & getTranslation() {return _translation;}
+    //@{
+    /// Return an internal reference to the translation part of the transform as an Extent2D
+    Extent2D const & getTranslation() const { return _translation; }
+    Extent2D & getTranslation() { return _translation; }
+    //@}
 
-    LinearTransform const & getLinear() const {return _linear;}
-    LinearTransform & getLinear() {return _linear;}
+    //@{
+    /// Return an internal reference to the upper 2x2 part of the transform as a LinearTransform
+    LinearTransform const & getLinear() const { return _linear; }
+    LinearTransform & getLinear() { return _linear; }
+    //@}
 
+    /// Return the transform matrix as an Eigen object.
     Matrix const getMatrix() const;
-    
+
+    /// Return the matrix elements as a flattened vector, ordered according to the Parameters enum.
     ParameterVector const getParameterVector() const;
+
+    /// Set the matrix elements from a flattened vector, ordered according to the Parameters enum.
     void setParameterVector(ParameterVector const & vector);
 
-    double & operator[](int i) { 
-        return (i < 4) ? _linear[i] : _translation[i - 4]; 
+    //@{
+    /// Return an element of the transform matrix, as indexed by the Parameters enum.
+    double & operator[](int i) {
+        return (i < 4) ? _linear[i] : _translation[i - 4];
     }
-    double operator[](int i) const { 
-        return (i < 4) ? _linear[i] : _translation[i - 4]; 
+    double operator[](int i) const {
+        return (i < 4) ? _linear[i] : _translation[i - 4];
     }
+    //@}
 
+    /// Composition of transforms (just matrix multiplication).
     AffineTransform operator*(AffineTransform const & other) const {
         return AffineTransform(
             getLinear()*other.getLinear(),
@@ -170,95 +186,93 @@ public:
         );
     }
 
-    AffineTransform & operator =(AffineTransform const & other) {
+    /// Standard assignment.
+    AffineTransform & operator=(AffineTransform const & other) {
         _linear = other._linear;
         _translation = other._translation;
         return *this;
     }
 
     /**
-     *  \brief Construct a new AffineTransform that represents a uniform scaling.
+     *  @brief Construct a new AffineTransform that represents a uniform scaling.
      *
-     *  \return An AffineTransform with matrix
-     *  \f$
+     *  @return An AffineTransform with matrix
+     *  @f[
      *     \left[\begin{array}{ c c c }
      *     s & 0 & 0 \\
      *     0 & s & 0 \\
      *     0 & 0 & 1 \\
      *     \end{array}\right]
-     *  \f$
+     *  @f]
      */
-    static AffineTransform makeScaling(double s) { 
+    static AffineTransform makeScaling(double s) {
         return AffineTransform(LinearTransform::makeScaling(s));
     }
 
     /**
-     *  \brief Construct a new AffineTransform that represents a non-uniform 
-     *  scaling.
+     *  @brief Construct a new AffineTransform that represents a scaling along the coordinate axes.
      *
-     *  \return An AffineTransform with matrix
-     *  \f$
+     *  @return An AffineTransform with matrix
+     *  @f[
      *     \left[\begin{array}{ c c c }
      *     s & 0 & 0 \\
      *     0 & t & 0 \\
      *     0 & 0 & 1 \\
      *     \end{array}\right]
-     *  \f$
+     *  @f]
      */
-    static AffineTransform makeScaling(double s, double t) { 
+    static AffineTransform makeScaling(double s, double t) {
         return AffineTransform(LinearTransform::makeScaling(s, t));
     }
     /**
-     *  \brief Construct a new AffineTransform that represents a CCW rotation in radians.
+     *  @brief Construct a new AffineTransform that represents a CCW rotation.
      *
-     *  \return An AffineTransform with matrix
-     *  \f$
+     *  @return An AffineTransform with matrix
+     *  @f[
      *     \left[\begin{array}{ c c c }
      *     \cos t & -\sin t & 0 \\
      *     \sin t & \cos t & 0  \\
      *     0 & 0 & 1 \\
      *     \end{array}\right]
-     *  \f$
+     *  @f]
      */
-    static AffineTransform makeRotation(double t) { 
+    static AffineTransform makeRotation(Angle t) {
         return AffineTransform(LinearTransform::makeRotation(t));
     }
 
     /**
-     *  \brief Construct a new AffineTransform that represents a pure translation.
+     *  @brief Construct a new AffineTransform that represents a pure translation.
      *
-     *  \return An AffineTransform with matrix
-     *  \f$
+     *  @return An AffineTransform with matrix
+     *  @f[
      *     \left[\begin{array}{ c c c }
      *     0 & 0 & translation.getX() \\
      *     0 & 0 & translation.getY() \\
      *     0 & 0 & 1 \\
      *     \end{array}\right]
-     *  \f$
+     *  @f]
      */
-    static AffineTransform makeTranslation(Extent2D translation) { 
+    static AffineTransform makeTranslation(Extent2D translation) {
         return AffineTransform(translation);
     }
 
+    //@{
+    /// Return the derivative of the transformed point with respect to the transform matrix elements.
     TransformDerivativeMatrix dTransform(Point2D const & input) const;
     TransformDerivativeMatrix dTransform(Extent2D const & input) const;
+    //@}
 
 private:
-
     LinearTransform _linear;
     Extent2D _translation;
 };
 
 std::ostream & operator<<(std::ostream & os, lsst::afw::geom::AffineTransform const & transform);
 
-
-//
-// Returns the unique AffineTransform A such that A(p_i)=q_i for i=1,2,3
-//
+/// Returns the unique AffineTransform A such that A(p_i)=q_i for i=1,2,3
 AffineTransform makeAffineTransformFromTriple(Point2D const &p1, Point2D const &p2, Point2D const &p3,
                                               Point2D const &q1, Point2D const &q2, Point2D const &q3);
 
+}}} // namespace lsst::afw::geom
 
-}}}
-
-#endif // !LSST_AFW_MATH_AFFINE_TRANSFORM_H
+#endif // !LSST_AFW_GEOM_AFFINE_TRANSFORM_H
--- include/lsst/afw/geom/LinearTransform.h
+++ include/lsst/afw/geom/LinearTransform.h
@@ -1,7 +1,8 @@
-/* 
+// -*- lsst-c++ -*-
+/*
  * LSST Data Management System
- * Copyright 2008, 2009, 2010 LSST Corporation.
- * 
+ * Copyright 2008-2013 LSST Corporation.
+ *
  * This product includes software developed by the
  * LSST Project (http://www.lsst.org/).
  *
@@ -9,39 +10,42 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
- * You should have received a copy of the LSST License Statement and 
- * the GNU General Public License along with this program.  If not, 
+ *
+ * You should have received a copy of the LSST License Statement and
+ * the GNU General Public License along with this program.  If not,
  * see <http://www.lsstcorp.org/LegalNotices/>.
  */
- 
-#ifndef LSST_AFW_GEOM_LINEAR_TRANSFORM_H 
+
+#ifndef LSST_AFW_GEOM_LINEAR_TRANSFORM_H
 #define LSST_AFW_GEOM_LINEAR_TRANSFORM_H
 
 #include "Eigen/Core"
 #include "Eigen/Geometry"
 #include "lsst/pex/exceptions/Runtime.h"
 
+#include "lsst/afw/geom/Angle.h"
 #include "lsst/afw/geom/Point.h"
 
-namespace lsst {
-namespace afw {
-namespace geom {
+namespace lsst { namespace afw { namespace geom {
 
 #ifndef SWIG
-    LSST_EXCEPTION_TYPE(SingularTransformException, lsst::pex::exceptions::RuntimeErrorException, lsst::afw::geom::SingularTransformException)
+    LSST_EXCEPTION_TYPE(
+        SingularTransformException,
+        lsst::pex::exceptions::RuntimeErrorException,
+        lsst::afw::geom::SingularTransformException
+    )
 #endif
 
 /**
- *  \brief A 2D linear coordinate transformation.
+ *  @brief A 2D linear coordinate transformation.
  *
- *  The transform is represented by a matrix \f$ \mathbf{M} \f$ such that
- *  \f[
+ *  The transform is represented by a matrix @f$ \mathbf{M} @f$ such that
+ *  @f[
  *     \left[\begin{array}{ c }
  *     x_f \\
  *     y_f
@@ -52,101 +56,151 @@ namespace geom {
  *     x_i \\
  *     y_i
  *     \end{array}\right]
- *  \f]
- *  where \f$(x_i,y_i)\f$ are the input coordinates and \f$(x_f,y_f)\f$ are 
+ *  @f]
+ *  where @f$(x_i,y_i)@f$ are the input coordinates and @f$(x_f,y_f)@f$ are
  *  the output coordinates.
  *
- *  If \f$ x_f(x_i,y_i) \f$ and \f$ y_f(x_i,y_i) \f$ are continuous 
+ *  If @f$ x_f(x_i,y_i) @f$ and @f$ y_f(x_i,y_i) @f$ are continuous
  *  differentiable functions, then
- *  \f[
+ *  @f[
  *     \mathbf{M} = \left[\begin{array}{ c c }
  *     \displaystyle\frac{\partial x_f}{\partial x_i} &
  *     \displaystyle\frac{\partial x_f}{\partial y_i} \\
  *     \displaystyle\frac{\partial y_f}{\partial x_i} &
  *     \displaystyle\frac{\partial y_f}{\partial y_i}
  *     \end{array}\right]
- *  \f]
- *  evaluated at \f$(x_i,y_i)\f$.
+ *  @f]
+ *  evaluated at @f$(x_i,y_i)@f$.
  */
 class LinearTransform {
 public:
+    /// Enum to provide indices for matrix elements when we have to flatten them (e.g. for derivatives)
     enum Parameters {XX=0,YX=1,XY=2,YY=3};
 
+    /// Type used when flattening matrix elements into a vector.
     typedef Eigen::Matrix<double,4,1> ParameterVector;
+
+    /// Type used when computing the derivative of a transformed point with respect to the transform.
     typedef Eigen::Matrix<double,2,4> TransformDerivativeMatrix;
+
+    /// Type used when computing the derivative of the product of two transforms.
     typedef Eigen::Matrix<double,4,4> ProductDerivativeMatrix;
 
+    /// Matrix type used to define the transform itself.
     typedef Eigen::Matrix<double,2,2,Eigen::DontAlign> Matrix;
 
-    /** \brief Construct an empty (identity) LinearTransform. */
+    /// Construct an identity LinearTransform.
     LinearTransform() : _matrix(Matrix::Identity()) {}
 
-    /** \brief Construct an LinearTransform from an Eigen::Matrix. */
+    /// Construct an LinearTransform from an Eigen::Matrix.
     explicit LinearTransform(Matrix const & matrix) : _matrix(matrix) {}
 
+    /// Composition of transforms (just matrix multiplication)
     LinearTransform operator*(LinearTransform const & other) const {
         return LinearTransform(getMatrix() * other.getMatrix());
     }
 
-
-    static LinearTransform makeScaling(double s) { 
+    /**
+     *  @brief Create a LinearTransform that represents a uniform scaling.
+     *
+     *  @return A LinearTransform with matrix
+     *  @f[
+     *     \left[\begin{array}{ c c }
+     *     s & 0 \\
+     *     0 & s
+     *     \end{array}\right]
+     *  @f]
+     */
+    static LinearTransform makeScaling(double s) {
         return LinearTransform((Matrix() << s, 0.0, 0.0, s).finished());
     }
-    
-    static LinearTransform makeScaling(double s, double t) { 
+
+    /**
+     *  @brief Create a LinearTransform that represents a scaling along the coordinate axes.
+     *
+     *  @return A LinearTransform with matrix
+     *  @f[
+     *     \left[\begin{array}{ c c }
+     *     s & 0 \\
+     *     0 & t
+     *     \end{array}\right]
+     *  @f]
+     */
+    static LinearTransform makeScaling(double s, double t) {
         return LinearTransform((Matrix() << s, 0.0, 0.0, t).finished());
     }
 
-    static LinearTransform makeRotation(double t) { 
-        return LinearTransform(Matrix(Eigen::Rotation2D<double>(t)));
+    /**
+     *  @brief Create a LinearTransform that represents a counter-clockwise rotation
+     *
+     *  @return A LinearTransform with matrix
+     *  @f[
+     *     \left[\begin{array}{ c c }
+     *     \cos t & -\sin t \\
+     *     \sin t & \cos t
+     *     \end{array}\right]
+     *  @f]
+     */
+    static LinearTransform makeRotation(Angle t) {
+        return LinearTransform(Matrix(Eigen::Rotation2D<double>(t.asRadians())));
     }
-    
+
+    /// Standard assignment.
     LinearTransform & operator=(LinearTransform const & other) {
         _matrix = other._matrix;
         return *this;
     }
-    
+
+    /// Return the matrix elements as a flattened vector, ordered according to the Parameters enum.
     ParameterVector const getParameterVector() const;
+
+    /// Set the matrix elements from a flattened vector, ordered according to the Parameters enum.
     void setParameterVector(ParameterVector const & vector);
-    
+
+    //@{
+    /// Return a reference to the transform matrix as an Eigen object.
     Matrix const & getMatrix() const { return _matrix; }
     Matrix & getMatrix() { return _matrix; }
+    //@}
 
+    //@{
+    /// Return an element of the transform matrix, as indexed by the Parameters enum.
     double & operator[](int i) { return _matrix(i % 2, i / 2); }
-    double const & operator[](int i) const { 
+    double const & operator[](int i) const {
         return const_cast<Matrix&>(_matrix)(i % 2, i / 2);
     }
+    //@}
 
-
+    /// Invert the transform (just matrix inversion).
     LinearTransform const invert() const;
-    
+
+    /// Return the determinant of the transform matrix.
     double computeDeterminant() const;
 
-    /** \brief Whether the transform is a no-op. */
+    /// Whether the transform is a no-op.
     bool isIdentity() const { return getMatrix().isIdentity(); }
 
     /**
-     *  \brief Transform a Point2D object. 
+     *  @brief Transform a Point2D object.
      *
-     *  This operation is equivalent to applying the LinearTransform to an
-     *  lsst::afw::geom::Extent
+     *  This operation is equivalent to applying the LinearTransform to an lsst::afw::geom::Extent
      */
     Point2D operator()(Point2D const & p) const { return Point2D(getMatrix() * p.asEigen()); }
 
     /**
-     *  \brief Transform a Extent2D object. 
+     *  @brief Transform a Extent2D object.
      *
-     *  This operation is equivalent to applying the LinearTransform to an
-     *  lsst::afw::geom::Point
+     *  This operation is equivalent to applying the LinearTransform to an lsst::afw::geom::Point
      */
     Extent2D operator()(Extent2D const & p) const { return Extent2D(getMatrix() * p.asEigen()); }
 
+    //@{
+    /// Return the derivative of the transformed point with respect to the transform matrix elements.
     TransformDerivativeMatrix dTransform(Point2D const & input) const;
-
-    /// Derivative of (*this)(input) with respect to the transform elements (for Extent);
     TransformDerivativeMatrix dTransform(Extent2D const & input) const {
         return dTransform(Point2D(input));
     }
+    //@}
 
 private:
     Matrix _matrix;
