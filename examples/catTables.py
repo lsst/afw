8484cc8a6a06104dc9258dc978bb6e22f195d828
Paul Price <price@astro.princeton.edu>
2013-05-31 14:09:46 -0400

    catTables: don't read all catalogs into memory at once
    We still need the total number of rows so we can preallocate the
    output catalog; otherwise the continual reallocating causes the
    memory to grow out of control.  Count the number of rows using
    pyfits if available; otherwise we have to read everything twice.

--- examples/catTables.py
+++ examples/catTables.py
@@ -3,23 +3,52 @@
 import sys
 import lsst.afw.table as afwTable
 
+def readCatalog(catalog):
+    """Read a catalog into memory"""
+    return afwTable.BaseCatalog.readFits(catalog) if isinstance(catalog, basestring) else catalog
 
-def concatenate(catalogList):
+def getCatalogRows(catalog, hdu=1):
+    """Return number of rows in a catalog"""
+    if not isinstance(catalog, basestring):
+        return len(catalog)
+    try:
+        # I believe this is significantly faster than reading the whole thing
+        import pyfits
+        fits = pyfits.open(catalog)
+        try:
+            rows = len(fits[hdu].data)
+            return rows
+        finally:
+            fits.close()
+    except ImportError:
+        # We have to read the whole thing just to get the length
+        return len(afwTable.BaseCatalog.readFits(catalog))
+
+def concatenate(catalogList, doPreallocate=True):
     """Concatenate multiple catalogs (FITS tables from lsst.afw.table)"""
-    catalogList = [afwTable.BaseCatalog.readFits(c) if isinstance(c, basestring) else c for c in catalogList]
 
-    schema = catalogList[0].schema
-    for i, c in enumerate(catalogList[1:]):
-        if c.schema != schema:
-            raise RuntimeError("Schema for catalog %d not consistent" % (i+1))
+    if doPreallocate:
+        num = sum(getCatalogRows(cat) for cat in catalogList)
+        print "%d rows from %d catalogs" % (num, len(catalogList))
+
+    cat = readCatalog(catalogList[0])
+    schema = cat.schema
 
     out = afwTable.BaseCatalog(schema)
-    num = reduce(lambda n, c: n + len(c), catalogList, 0)
-    out.preallocate(num)
+    table = out.table
 
-    for catalog in catalogList:
-        for record in catalog:
-            out.append(out.table.copyRecord(record))
+    if doPreallocate:
+        table.preallocate(num)
+        out.reserve(num)
+
+    for i, cat in enumerate(catalogList):
+        print cat
+        cat = readCatalog(cat)
+        if cat.schema != schema:
+            raise RuntimeError("Schema for catalog %d not consistent" % (i+1))
+        for record in cat:
+            out.append(table.copyRecord(record))
+        del cat
 
     return out
 
