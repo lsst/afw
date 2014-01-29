f54b749c87d10a7c2b4c3114df7e5f97cfba5cc1
Russell Owen <rowen@uw.edu>
2014-01-29 14:55:56 -0800

    TransformRegistry tests finished and passing.

--- tests/testCameraTransformRegistry.py
+++ tests/testCameraTransformRegistry.py
@@ -23,8 +23,7 @@
 """
 Tests for lsst.afw.cameraGeom.CameraTransformRegistry
 """
-
-import sys
+import itertools
 import unittest
 
 import lsst.utils.tests
@@ -32,18 +31,60 @@ import lsst.pex.exceptions as pexException
 import lsst.afw.geom as afwGeom
 import lsst.afw.cameraGeom as cameraGeom
 
+class ConversionFunction(object):
+    """Wrap a TransformRegistry conversion as a function(Point2D)->Point2D
+    """
+    def __init__(self, transformRegistry, fromSys, toSys):
+        self.transformRegistry = transformRegistry
+        self.fromSys = fromSys
+        self.toSys = toSys
+
+    def __call__(self, point):
+        return self.transformRegistry.convert(point, self.fromSys, self.toSys)
+
+class FuncPair(object):
+    """Wrap a pair of function(Point2D)->Point2D functions as a single such function
+    """
+    def __init__(self, func1, func2):
+        self.func1 = func1
+        self.func2 = func2
+
+    def __call__(self, point):
+        return self.func2(self.func1(point))
+
+def unityTransform(point):
+    """Unity function(Point2D)->Point2D
+    """
+    return point
+
 
 class CameraTransformRegistryTestCase(unittest.TestCase):
     def setUp(self):
         self.nativeSys = cameraGeom.FOCAL_PLANE
-        self.radialTransform = afwGeom.RadialXYTransform([0, 0.5, 0.01])
-        transMap = {cameraGeom.PUPIL: self.radialTransform}
+        self.pupilTransform = afwGeom.RadialXYTransform([0, 0.5, 0.01])
+        transMap = {cameraGeom.PUPIL: self.pupilTransform}
         self.transReg = cameraGeom.CameraTransformRegistry(self.nativeSys, transMap)
 
     def tearDown(self):
         self.nativeSys = None
-        self.radialTransform = None
+        self.pupilTransform = None
         self.transReg = None
+
+    def compare2DFunctions(self, func1, func2, minVal=-10, maxVal=None, nVal=5):
+        """Compare two functions(Point2D) -> Point2D over a range of values
+        """
+        if maxVal is None:
+            maxVal = -minVal
+        dVal = (maxVal - minVal) / (nVal - 1)
+        for xInd in range(nVal):
+            x = minVal + (xInd * dVal)
+            for yInd in range(nVal):
+                y = minVal + (yInd * dVal)
+                fromPoint = afwGeom.Point2D(x, y)
+                res1 = func1(fromPoint)
+                res2 = func2(fromPoint)
+                self.assertAlmostEqual(res1[0], res2[0])
+                self.assertAlmostEqual(res1[1], res2[1])
 
     def testBasics(self):
         """Test basic attributes
@@ -60,9 +101,6 @@ class CameraTransformRegistryTestCase(unittest.TestCase):
 
     def testIteration(self):
         """Test iteration, len and indexing
-
-        I don't bother to test that the returned transforms are the right once
-        because it's more work than it's worth
         """
         self.assertEquals(len(self.transReg), 2)
         trList = []
@@ -74,6 +112,31 @@ class CameraTransformRegistryTestCase(unittest.TestCase):
 
         for missingSys in (cameraGeom.DetectorSysPrefix("foo"), cameraGeom.CameraSys("garbage")):
             self.assertRaises(pexException.LsstCppException, self.transReg.__getitem__, missingSys)
+
+    def testTransforms(self):
+        """Test that the contained transforms are the ones expected
+        """
+        nativeTr = self.transReg[self.nativeSys]
+        self.compare2DFunctions(nativeTr.forwardTransform, unityTransform)
+        self.compare2DFunctions(nativeTr.reverseTransform, unityTransform)
+
+        pupilTr = self.transReg[cameraGeom.PUPIL]
+        self.compare2DFunctions(pupilTr.forwardTransform, self.pupilTransform.forwardTransform)
+        self.compare2DFunctions(pupilTr.reverseTransform, self.pupilTransform.reverseTransform)
+
+    def testConvert(self):
+        """Test convert
+        """
+        for fromSys in self.transReg.getCoordSysList():
+            for toSys in self.transReg.getCoordSysList():
+                trConvFunc = ConversionFunction(self.transReg, fromSys, toSys)
+                if fromSys == toSys:
+                    self.compare2DFunctions(trConvFunc, unityTransform)
+                funcPair = FuncPair(
+                    self.transReg[fromSys].forwardTransform,
+                    self.transReg[toSys].reverseTransform,
+                )
+                self.compare2DFunctions(trConvFunc, funcPair)
 
 
 #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
