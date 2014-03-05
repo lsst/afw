322ab936a503294f4239a71591f68cdf4358858c
Russell Owen <rowen@uw.edu>
2014-03-05 09:50:34 -0800

    Move rotateBBoxBy90 out of __init__.py Change run methods in CameraFactoryTask to share more code.

--- python/lsst/afw/cameraGeom/cameraFactoryTask.py
+++ python/lsst/afw/cameraGeom/cameraFactoryTask.py
@@ -1,8 +1,10 @@
 import os.path
 import lsst.afw.geom as afwGeom
 from lsst.afw.table import AmpInfoCatalog
-from lsst.afw.cameraGeom import FOCAL_PLANE, PUPIL, PIXELS, ACTUAL_PIXELS, CameraSys,\
-                                Camera, Detector, Orientation, CameraTransformMap
+from .cameraGeomLib import FOCAL_PLANE, PUPIL, PIXELS, TAN_PIXELS, ACTUAL_PIXELS, CameraSys, \
+    Detector, Orientation, CameraTransformMap
+from .camera import Camera
+from .makePixelToTanPixel import makePixelToTanPixel
 
 __all__ = ["CameraFactoryTask"]
 
@@ -12,7 +14,7 @@ class CameraFactoryTask(object):
     Eventually we hope that camera data will be unpersisted using a butler,
     which is why this is written to look something like a Task.
     """
-    cameraSysList = [PUPIL, FOCAL_PLANE, PIXELS, ACTUAL_PIXELS]
+    cameraSysList = [PUPIL, FOCAL_PLANE, PIXELS, TAN_PIXELS, ACTUAL_PIXELS]
     cameraSysMap = dict((sys.getSysName(), sys) for sys in cameraSysList)
 
     def __init__(self):
@@ -24,10 +26,10 @@ class CameraFactoryTask(object):
         """Construct a camera (lsst.afw.cameraGeom Camera)
 
         @param[in] cameraConfig: an instance of CameraConfig
-        @param[in] ampInfoPath: path to find the persisted AmpInfoCatalogs
+        @param[in] ampInfoPath: path to ampInfo data files
         @return camera (an lsst.afw.cameraGeom.Camera)
         """
-        detectorList = []
+        ampInfoCatDict = dict()
         for detectorConfig in cameraConfig.detectorList.itervalues():
             #HACK until the mapper can get the short name
             nameEls = detectorConfig.name.split(" ")
@@ -37,12 +39,10 @@ class CameraFactoryTask(object):
             else:
                 shortName = "R%s%s_S%s%s"%(nameEls[0][2], nameEls[0][4], nameEls[1][2], nameEls[1][4])
             ampCatPath = os.path.join(ampInfoPath, shortName + ".fits")
-            ampInfoCat = AmpInfoCatalog.readFits(ampCatPath)
-            detectorList.append(self.makeDetector(detectorConfig, ampInfoCat))
-        nativeSys = self.cameraSysMap[cameraConfig.transformDict.nativeSys]
-        transformDict = self.makeTransformDict(cameraConfig.transformDict.transforms)
-        transformMap = CameraTransformMap(nativeSys, transformDict)
-        return Camera(cameraConfig.name, detectorList, transformMap)
+            ampInfoCatalog = AmpInfoCatalog.readFits(ampCatPath)
+            ampInfoCatDict[detectorConfig.name] = ampInfoCatalog
+
+        return self.runCatDict(self, cameraConfig, ampInfoCatDict)
 
     def runCatDict(self, cameraConfig, ampInfoCatDict):
         """Construct a camera (lsst.afw.cameraGeom Camera)
@@ -51,30 +51,60 @@ class CameraFactoryTask(object):
         @param[in] ampInfoCatDict: a dictionary keyed on the detector name of AmpInfoCatalog objects
         @return camera (an lsst.afw.cameraGeom.Camera)
         """
-        detectorList = []
-        for detectorConfig in cameraConfig.detectorList.itervalues():
-            detectorList.append(self.makeDetector(detectorConfig, ampInfoCatDict[detectorConfig.name]))
         nativeSys = self.cameraSysMap[cameraConfig.transformDict.nativeSys]
         transformDict = self.makeTransformDict(cameraConfig.transformDict.transforms)
+        focalPlaneToPupil = transformDict[PUPIL]
         transformMap = CameraTransformMap(nativeSys, transformDict)
+
+        detectorList = []
+        for detectorConfig in cameraConfig.detectorList.itervalues():
+            ampInfoCatalog = ampInfoCatDict[detectorConfig.name]
+
+            detectorList.append(self.makeDetector(
+                detectorConfig = detectorConfig,
+                ampInfoCatalog = ampInfoCatalog,
+                focalPlaneToPupil = focalPlaneToPupil,
+                plateScale = cameraConfig.plateScale,
+            ))
+
         return Camera(cameraConfig.name, detectorList, transformMap)
 
-    def makeDetector(self, detectorConfig, ampInfoCatalog):
+    def makeDetector(self, detectorConfig, ampInfoCatalog, focalPlaneToPupil, plateScale):
         """Make a detector object:
 
         @param detectorConfig -- config for this detector (an lsst.pex.config.Config)
         @param ampInfoCatalog -- amplifier information for this detector (an lsst.afw.table.AmpInfoCatalog)
+        @param focalPlaneToPupil -- FOCAL_PLANE to PUPIL XYTransform
+        @param plateScale -- nominal plate scale (arcsec/mm)
         @return detector (an lsst.afw.cameraGeom.Detector)
         """
         orientation = self.makeOrientation(detectorConfig)
-        pixelSize = afwGeom.Extent2D(detectorConfig.pixelSize_x, detectorConfig.pixelSize_y)
+        pixelSizeMm = afwGeom.Extent2D(detectorConfig.pixelSize_x, detectorConfig.pixelSize_y)
         transforms = self.makeTransformDict(detectorConfig.transformDict.transforms)
-        transforms[FOCAL_PLANE] = orientation.makePixelFpTransform(pixelSize)
+        transforms[FOCAL_PLANE] = orientation.makePixelFpTransform(pixelSizeMm)
+
         llPoint = afwGeom.Point2I(detectorConfig.bbox_x0, detectorConfig.bbox_y0)
         urPoint = afwGeom.Point2I(detectorConfig.bbox_x1, detectorConfig.bbox_y1)
         bbox = afwGeom.Box2I(llPoint, urPoint)
-        return Detector(detectorConfig.name, detectorConfig.detectorType, detectorConfig.serial, bbox, ampInfoCatalog, 
-                             orientation, pixelSize, transforms)
+
+        transforms[TAN_PIXELS] = makePixelToTanPixel(
+            bbox = bbox,
+            orientation = orientation,
+            focalPlaneToPupil = focalPlaneToPupil,
+            pixelSizeMm = pixelSizeMm,
+            plateScale = plateScale,
+        )
+
+        return Detector(
+            detectorConfig.name,
+            detectorConfig.detectorType,
+            detectorConfig.serial,
+            bbox,
+            ampInfoCatalog,
+            orientation,
+            pixelSizeMm,
+            transforms,
+        )
 
     def makeOrientation(self, detectorConfig):
         """Make an instance of an Orientation class
