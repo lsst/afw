b619c24a5f5843753b26478e5410fddf7391a019
Jim Bosch <jbosch@astro.princeton.edu>
2014-08-25 01:05:02 -0400

    Need to apply aliases in SubSchema implicit conversions

--- include/lsst/afw/table/Schema.h
+++ include/lsst/afw/table/Schema.h
@@ -381,7 +381,7 @@ public:
      *  wrappers provide an equivalent asKey() method.
      */
     template <typename T>
-    operator Key<T>() const { return _impl->find<T>(_name).key; }
+    operator Key<T>() const { return _impl->find<T>(_aliases->apply(_name)).key; }
 
     /**
      *  @brief Implicit conversion to the appropriate Key type.
@@ -390,7 +390,7 @@ public:
      *  wrappers provide an equivalent asField() method.
      */
     template <typename T>
-    operator Field<T>() const { return _impl->find<T>(_name).field; }
+    operator Field<T>() const { return _impl->find<T>(_aliases->apply(_name)).field; }
 
 private:
 
