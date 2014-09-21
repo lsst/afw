cd4ec887728ab05e23194c35a9491f64a8fde614
Jim Bosch <jbosch@astro.princeton.edu>
2014-09-21 12:14:31 -0400

    Documentation for FunctorKey base classes.

--- include/lsst/afw/table/FunctorKey.h
+++ include/lsst/afw/table/FunctorKey.h
@@ -28,6 +28,12 @@
 
 namespace lsst { namespace afw { namespace table {
 
+/**
+ *  Base class for objects that can extract a value from a record, but are not a true Key themselves.
+ *
+ *  Objects that inherit from OutputFunctorKey can be passed to BaseRecord::get(), just as true Keys can,
+ *  but the record will simply pass itself to OutputFunctorKey::get() and return the result.
+ */
 template <typename T>
 class OutputFunctorKey {
 public:
@@ -38,6 +44,12 @@ public:
 
 };
 
+/**
+ *  Base class for objects that can set a value on a record, but are not a true Key themselves.
+ *
+ *  Objects that inherit from InputFunctorKey can be passed to BaseRecord::set(), just as true Keys can,
+ *  but the record will simply pass itself to OutputFunctorKey::set() along with the value that was passed.
+ */
 template <typename T>
 class InputFunctorKey {
 public:
@@ -48,9 +60,25 @@ public:
 
 };
 
+/**
+ *  Convenience base class that combines the OutputFunctorKey and InputFunctorKey
+ *
+ *  Most objects that can set a calculated or compound value from a record can also get that value back,
+ *  so we provide this class to aggregate those interfaces.
+ */
 template <typename T>
 class FunctorKey : public OutputFunctorKey<T>, public InputFunctorKey<T> {};
 
+/**
+ *  Base class for objects that can return a non-const reference to part of a record, but are not a true Key
+ *
+ *  Objects that inherit from ReferenceFunctorKey can be passed to BaseRecord::operator[], just as true Keys
+ *  can, but the record will simply pass itself to ReferenceFunctorKey::getReference().
+ *
+ *  @note We'd combine this with the ConstReferenceFunctorKey interface if it weren't for the fact that
+ *  we can't pass multiple template arguments to a Swig macro if either contains commas, and we'd need that
+ *  to wrap a combined interface base class.
+ */
 template <typename T>
 class ReferenceFunctorKey {
 public:
@@ -63,6 +91,16 @@ public:
 
 };
 
+/**
+ *  Base class for objects that can return a const reference to part of a record, but are not a true Key
+ *
+ *  Objects that inherit from ConstReferenceFunctorKey can be passed to BaseRecord::operator[], just as true
+ *  Keys can, but the record will simply pass itself to ReferenceFunctorKey::getConstReference().
+ *
+ *  @note We'd combine this with the ReferenceFunctorKey interface if it weren't for the fact that
+ *  we can't pass multiple template arguments to a Swig macro if either contains commas, and we'd need that
+ *  to wrap a combined interface base class.
+ */
 template <typename T>
 class ConstReferenceFunctorKey {
 public:
