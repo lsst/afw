fe33bbcafca963dd6cfadda02adb1bbc11673af5
SimonKrughoff <simon.krughoff@gmail.com>
2015-04-17 15:05:03 -0700

    Fix small bug in findDetectorsArray
    The points were being passed to the transform map as CameraPoints and they should have been Point2ds

--- python/lsst/afw/cameraGeom/camera.py
+++ python/lsst/afw/cameraGeom/camera.py
@@ -139,7 +139,7 @@ class Camera(DetectorCollection):
         corresponding point
         """
 
-        cameraPoints = [self.makeCameraPoint(afwGeom.Point2d(x,y),coordSys) for x,y in zip(xCoord, yCoord)]
+        cameraPoints = [afwGeom.Point2d(x,y) for x,y in zip(xCoord, yCoord)]
 
         #transform the points to the native coordinate system
         nativePoints = _transformSingleSysArray(cameraPoints, coordSys, self._nativeCameraSys)
