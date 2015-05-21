ed95653aae0db5c64fec82321af822db861c9e9c
Joshua Hoblitt <josh@hoblitt.com>
2015-05-21 14:38:34 -0700

    replace eups.productDir() calls with lsst.utils.getPackageDir()

--- lib/libafw.so-gdb.py
+++ lib/libafw.so-gdb.py
@@ -1,12 +1,18 @@
 import os, os.path, sys
+import lsst.utils
 import gdb
 #
 # Adjust the load path to include lsst.gdb, bypassing the regular lsstimport mechanism as
 # the version of python running within gdb may not be the same as we are using for lsst processing
 #
-printerDir = os.path.join(os.environ["AFW_DIR"], "python", "lsst", "gdb")
-if printerDir not in sys.path:
-    sys.path.append(printerDir)
+try:
+    afwDir = lsst.utils.getPackageDir('afw')
+except Exception:
+    pass
+else:
+    printerDir = os.path.join(afwDir, "python", "lsst", "gdb")
+    if printerDir not in sys.path:
+        sys.path.append(printerDir)
 
 import afw.printers
 
