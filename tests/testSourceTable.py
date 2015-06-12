376215ecec00d7342d1dc3c815b5276946160d4b
Paul Price <price@astro.princeton.edu>
2015-06-12 16:47:37 -0400

    SourceCatalog: test assumption on sort order for getChildren
    SourceCatalog.getChildren assumes that the catalog is sorted by
    parent, but did not check this assumption, which can lead to
    misleading results if the assumption is not true.  Test the
    assumption and raise an AssertionError if it fails.

--- tests/testSourceTable.py
+++ tests/testSourceTable.py
@@ -396,6 +396,12 @@ class SourceTableTestCase(lsst.utils.tests.TestCase):
                 self.assertEqual(child.getParent(), parent.getId())
                 self.assertEqual(child.getId(), id)
 
+        # Check detection of unsorted catalog
+        self.catalog.sort(self.fluxKey)
+        self.assertRaises(AssertionError, self.catalog.getChildren, 0)
+        self.catalog.sort(parentKey)
+        self.catalog.getChildren(0) # Just care this succeeds
+
     def testFitsReadBackwardsCompatibility(self):
         cat = lsst.afw.table.SourceCatalog.readFits("tests/data/empty-v0.fits")
         self.assertTrue(cat.getPsfFluxSlot().isValid())
