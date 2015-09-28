a65deab1fad3d3c8e680dd9fecf8e0ce585f71f5
danielsf <scott.f.daniel@gmail.com>
2015-09-28 10:49:57 -0700

    added a unit test to verify that the transformation between pupil coordinates and pixel coordinates behaves as expected

--- tests/testCameraRepoFactory.py
+++ tests/testCameraRepoFactory.py
@@ -103,6 +103,99 @@ class CameraRepoFactoryTest(unittest.TestCase):
                 self.assertAlmostEqual(float(yyPcontrol), pixelPoint.getY(), 9, msg=detName)
 
 
+    def testPupilCoords(self):
+        """
+        Test that cameras generated with CameraRepositoryFactory convert from pupil
+        coordinates to pixel coordinates as expected
+        """
+        layoutFile = os.path.join(self.cameraDataDir, 'testFocalPlaneLayout_0.txt')
+        segmentFile = os.path.join(self.cameraDataDir, 'testSegmentationFile_0.txt')
+
+        def getIdFromName(name):
+            return int(name[-2:])
+
+        np.random.seed(435)
+
+
+        detNameList = ['Det00', 'Det01', 'Det02']
+        yawList = np.radians([20.0, 10.0, 30.0])
+        xCenterList = [0.0, 0.0, -9.0]
+        yCenterList = [0.0, 9.0, 0.0]
+        mmPerPixel = 2.0e-3
+
+        for iteration in range(3):
+
+            # try random radial transforms between pupil and pixel coordinates
+            radial_coeffs = np.random.random_sample(5)
+            radial_coeffs[0] = 0.0
+
+            factory = CameraRepositoryFactory(detectorLayoutFile=layoutFile,
+                                              segmentationFile=segmentFile,
+                                              detectorIdFromAbbrevName=getIdFromName,
+                                              radialTransform = radial_coeffs,
+                                              detTypeMap = {'science':SCIENCE,
+                                                            'focus':FOCUS,
+                                                            'guider':GUIDER,
+                                                            'wave':WAVEFRONT})
+
+
+            camera = factory.makeCamera()
+
+            for detName, yaw, xCenter, yCenter in \
+            zip(detNameList, yawList, xCenterList, yCenterList):
+
+                pupilSystem = camera[detName].makeCameraSys(PUPIL)
+                pixelSystem = camera[detName].makeCameraSys(PIXELS)
+                focalSystem = camera[detName].makeCameraSys(FOCAL_PLANE)
+
+                # approximate radians per pixel
+                scale_approx = mmPerPixel/radial_coeffs[1]
+
+                for theta in np.arange(0.0, 2.0*np.pi, 0.5*np.pi):
+                    for rr in np.arange(10.0*scale_approx, 200.0*scale_approx, 50.0*scale_approx):
+
+                        xp = rr*np.cos(theta)
+                        yp = rr*np.sin(theta)
+
+                        rr_scale = np.array([radial_coeffs[ii]*rr**ii \
+                                             for ii in range(len(radial_coeffs))]).sum()/rr
+
+                        xf = rr_scale*rr*np.cos(theta)
+                        yf = rr_scale*rr*np.sin(theta)
+
+                        pupilPoint = camera.makeCameraPoint(afwGeom.Point2D(xp, yp), PUPIL)
+                        focalPoint = camera.transform(pupilPoint, focalSystem).getPoint()
+
+                        # Below we verify that the transformation from
+                        # pupil coordinates to focal plane coordinates
+                        # behaves the way we expect it to
+                        #
+                        # Multiplying by 10^10 achives an
+                        # approximate conversion from radians to 10^-4 arcsec
+                        dx = np.abs(xf-focalPoint.getX())*1.0e10
+                        self.assertAlmostEqual(dx, 0.0, 5)
+
+                        dy = np.abs(yf - focalPoint.getY())*1.0e10
+                        self.assertAlmostEqual(dy, 0.0, 5)
+
+                        # re-center on chip's origin
+                        xfCenter = xf - xCenter
+                        yfCenter = yf - yCenter
+
+                        # rotate by -yaw to orient to the x, y axes of the chip
+                        xfChip = xfCenter*np.cos(yaw) + yfCenter*np.sin(yaw)
+                        yfChip = -xfCenter*np.sin(yaw) + yfCenter*np.cos(yaw)
+
+                        yPix = 199.5 - xfChip/mmPerPixel
+                        xPix = 199.5 + yfChip/mmPerPixel
+
+                        pixelPoint = camera.transform(pupilPoint, pixelSystem).getPoint()
+
+                        self.assertAlmostEqual(xPix, pixelPoint.getX(), 10)
+                        self.assertAlmostEqual(yPix, pixelPoint.getY(), 10)
+
+
+
 def suite():
     """Returns a suite containing all the test cases in this module."""
 
