7978aefd7fd6221fc6d524d079a226476c524c93
Jim Bosch <jbosch@astro.princeton.edu>
2016-05-13 10:37:53 -0400

    Tweak last workaround (should squash).

--- include/lsst/afw/geom/TransformMapImpl.h
+++ include/lsst/afw/geom/TransformMapImpl.h
@@ -58,7 +58,7 @@ TransformMap<CoordSysT>::TransformMap(
     // insert identity transform for nativeCoordSys, if not already provided
     if (!contains(nativeCoordSys)) {
         // Avoiding std::make_pair here to work around probable XCode 6.2 standard library bug.
-        std::pair<CoordSysT,std::shared_ptr<IdentityXYTransform> pair(
+        std::pair<CoordSysT const,std::shared_ptr<IdentityXYTransform>> pair(
             nativeCoordSys,
             std::make_shared<IdentityXYTransform>()
         );
