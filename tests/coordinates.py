1a707f2078231a256337e5b4dca3c3e3e828bd95
Tim Jenness <tjenness@lsst.org>
2016-08-11 20:13:47 -0700

    Only test div and idiv operators on python 2

--- tests/coordinates.py
+++ tests/coordinates.py
@@ -385,10 +385,11 @@ class OperatorTestCase(utilsTests.TestCase):
             self.checkOperator(operator.mul, float, eI, eD)
             # Old-Style Division (note that operator.div doesn't obey the future statement; it just calls
             # __div__ directly.
-            self.checkOperator(operator.div, eD, int, eD)
-            self.checkOperator(operator.div, eD, float, eD)
-            self.checkOperator(operator.div, eI, int, eI)
-            self.checkOperator(operator.div, eI, float, eD)
+            if hasattr(operator, "div"):
+                self.checkOperator(operator.div, eD, int, eD)
+                self.checkOperator(operator.div, eD, float, eD)
+                self.checkOperator(operator.div, eI, int, eI)
+                self.checkOperator(operator.div, eI, float, eD)
             # New-Style Division
             self.checkOperator(operator.truediv, eD, int, eD)
             self.checkOperator(operator.truediv, eD, float, eD)
@@ -453,10 +454,11 @@ class OperatorTestCase(utilsTests.TestCase):
             self.checkOperator(operator.imul, eI, float, TypeError)
             # Old-Style Division (note that operator.div doesn't obey the future statement; it just calls
             # __div__ directly).
-            self.checkOperator(operator.idiv, eD, int, eD, inPlace=True)
-            self.checkOperator(operator.idiv, eD, float, eD, inPlace=True)
-            self.checkOperator(operator.idiv, eI, int, eI, inPlace=True)
-            self.checkOperator(operator.idiv, eI, float, TypeError)
+            if hasattr(operator, "idiv"):
+                self.checkOperator(operator.idiv, eD, int, eD, inPlace=True)
+                self.checkOperator(operator.idiv, eD, float, eD, inPlace=True)
+                self.checkOperator(operator.idiv, eI, int, eI, inPlace=True)
+                self.checkOperator(operator.idiv, eI, float, TypeError)
             # New-Style Division
             self.checkOperator(operator.itruediv, eD, int, eD, inPlace=True)
             self.checkOperator(operator.itruediv, eD, float, eD, inPlace=True)
