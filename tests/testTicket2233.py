396913c744bbda5811a0e3a6a59c8f8b94d10ef8
Pim Schellart <P.Schellart@princeton.edu>
2016-08-18 09:50:44 -0700

    Migrate testTicket2233 to pytest

--- tests/testTicket2233.py
+++ tests/testTicket2233.py
@@ -52,7 +52,7 @@ import unittest
 import pickle
 
 import lsst.afw.image as afwImage
-import lsst.utils.tests as utilsTests
+import lsst.utils.tests
 
 DATA = os.path.join("tests", "data", "ticket2233.fits")
 
@@ -74,22 +74,14 @@ class WcsFormatterTest(unittest.TestCase):
         self.assertEqual(wcs.getFitsMetadata().toString(), self.wcs.getFitsMetadata().toString())
 
 
-#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-
-def suite():
-    """Returns a suite containing all the test cases in this module."""
-    utilsTests.init()
-
-    suites = []
-    suites += unittest.makeSuite(WcsFormatterTest)
-    suites += unittest.makeSuite(utilsTests.MemoryTestCase)
-
-    return unittest.TestSuite(suites)
+class MemoryTester(lsst.utils.tests.MemoryTestCase):
+    pass
 
 
-def run(shouldExit=False):
-    """Run the tests"""
-    utilsTests.run(suite(), shouldExit)
+def setup_module(module):
+    lsst.utils.tests.init()
+
 
 if __name__ == "__main__":
-    run(True)
+    lsst.utils.tests.init()
+    unittest.main()
