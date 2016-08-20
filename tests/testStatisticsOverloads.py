5fda7f11daa677d67bcaaf509735ebbc16a7e8ca
fred3m <fredem@princeton.edu>
2016-08-20 12:08:33 -0700

    Update testStatisticsOveerloads boilerplate

--- tests/testStatisticsOverloads.py
+++ tests/testStatisticsOverloads.py
@@ -37,7 +37,7 @@ or
 
 import unittest
 
-import lsst.utils.tests as utilsTests
+import lsst.utils.tests
 import lsst.pex.exceptions
 import lsst.afw.image as afwImage
 import lsst.afw.geom as afwGeom
@@ -184,20 +184,12 @@ class StatisticsTestCase(unittest.TestCase):
 #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
 
-def suite():
-    """Returns a suite containing all the test cases in this module."""
+class TestMemory(lsst.utils.tests.MemoryTestCase):
+    pass
 
-    utilsTests.init()
-
-    suites = []
-    suites += unittest.makeSuite(StatisticsTestCase)
-    suites += unittest.makeSuite(utilsTests.MemoryTestCase)
-    return unittest.TestSuite(suites)
-
-
-def run(shouldExit=False):
-    """Run the tests"""
-    utilsTests.run(suite(), shouldExit)
+def setup_module(module):
+    lsst.utils.tests.init()
 
 if __name__ == "__main__":
-    run(True)
+    lsst.utils.tests.init()
+    unittest.main()
