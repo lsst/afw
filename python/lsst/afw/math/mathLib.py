342b492690b5d1e42e4ad6d695bfe2d50d8df2c5
fred3m <fredem@princeton.edu>
2016-10-07 15:07:41 -0400

    Wrap afw::math with pybind11

--- python/lsst/afw/math/mathLib.py
+++ python/lsst/afw/math/mathLib.py
@@ -1,1 +1,13 @@
 from __future__ import absolute_import
+
+import lsst.afw.geom
+
+from ._minimize import *
+from ._function import *
+from ._functionLibrary import *
+from ._interpolate import *
+from ._gaussianProcess import *
+from .spatialCell import *
+from ._boundedField import *
+from ._chebyshevBoundedField import *
+from ._leastSquares import *
