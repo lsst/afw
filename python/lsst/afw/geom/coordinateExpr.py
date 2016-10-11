268a80cc89f20d6eedba3d56974bd904727e4ea5
Pim Schellart <P.Schellart@princeton.edu>
2016-10-11 17:08:14 -0400

    Wrap afw::geom and afw::coord with pybind11
    - Create pybind11 stubs
    - Disable unported Python unit tests
    - Wrap afw::geom
    - Wrap afw::coord

---
+++ python/lsst/afw/geom/coordinateExpr.py
@@ -0,0 +1,5 @@
+from __future__ import absolute_import
+
+from ._coordinateExpr import *
+
+CoordinateExpr = {2:CoordinateExpr2, 3:CoordinateExpr3}
---
+++ python/lsst/afw/geom/geomLib.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import
+
+from .angle import *
+from ._coordinateBase import *
+from .coordinateExpr import *
+from .extent import *
+from .point import *
+from .box import *
+from ._functor import *
+from ._span import *
+from ._xYTransform import *
+from ._separableXYTransform import *
+from ._affineTransform import *
