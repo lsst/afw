277015431e51ee916e5c3bda30cb37cc08dac9b3
Krzysztof Findeisen <kfindeis@uw.edu>
2016-12-06 16:32:36 -0800

    Made lsst.afw.cameraGeom importable.
    This commit does not address any changes needed by specific tests. In
    particular, the template specialization CameraTransformMap =
    TransformMap<CameraSys> has not been handled in a way that's
    extensible.

--- python/lsst/afw/cameraGeom/cameraGeomLib.py
+++ python/lsst/afw/cameraGeom/cameraGeomLib.py
@@ -1,1 +1,7 @@
 from __future__ import absolute_import
+
+from lsst.afw.geom import Point2D
+from ._cameraPoint import *
+from ._cameraSys import *
+from ._detector import *
+from ._orientation import *
