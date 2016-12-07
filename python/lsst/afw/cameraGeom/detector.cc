321ccfb919da6c29dfbad0efb8bf76587e0f2786
Krzysztof Findeisen <kfindeis@uw.edu>
2016-12-06 16:32:36 -0800

    Made lsst.afw.cameraGeom importable.
    This commit does not address any changes needed by specific tests. In
    particular, the template specialization CameraTransformMap =
    TransformMap<CameraSys> has not been handled in a way that's
    extensible.

--- python/lsst/afw/cameraGeom/detector.cc
+++ python/lsst/afw/cameraGeom/detector.cc
@@ -1,7 +1,7 @@
-/* 
+/*
  * LSST Data Management System
- * Copyright 2008-2016  AURA/LSST.
- * 
+ * See COPYRIGHT file at the top of the source tree.
+ *
  * This product includes software developed by the
  * LSST Project (http://www.lsst.org/).
  *
@@ -9,14 +9,14 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
- * You should have received a copy of the LSST License Statement and 
- * the GNU General Public License along with this program.  If not, 
+ *
+ * You should have received a copy of the LSST License Statement and
+ * the GNU General Public License along with this program.  If not,
  * see <https://www.lsstcorp.org/LegalNotices/>.
  */
 
@@ -24,16 +24,27 @@
 //#include <pybind11/operators.h>
 //#include <pybind11/stl.h>
 
+#include "lsst/afw/cameraGeom/Detector.h"
+
 namespace py = pybind11;
 
-using namespace lsst::afw::cameraGeom;
+namespace lsst {
+namespace afw {
+namespace cameraGeom {
 
 PYBIND11_PLUGIN(_detector) {
     py::module mod("_detector", "Python wrapper for afw _detector library");
 
     /* Module level */
+    py::class_<Detector> clsDetector(mod, "Detector");
 
     /* Member types and enums */
+    py::enum_<DetectorType>(mod, "DetectorType")
+        .value("SCIENCE", DetectorType::SCIENCE)
+        .value("FOCUS", DetectorType::FOCUS)
+        .value("GUIDER", DetectorType::GUIDER)
+        .value("WAVEFRONT", DetectorType::WAVEFRONT)
+        .export_values();
 
     /* Constructors */
 
@@ -43,3 +54,5 @@ PYBIND11_PLUGIN(_detector) {
 
     return mod.ptr();
 }
+
+}}}
