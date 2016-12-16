aee38898205e7342c47fe46388162279605ad5cc
Pim Schellart <P.Schellart@princeton.edu>
2016-12-15 20:11:50 -0500

    Fix circular imports

--- python/lsst/afw/math/mathLib.py
+++ python/lsst/afw/math/mathLib.py
@@ -20,6 +20,6 @@ from ._offsetImage import *
 from ._stack import *
 from ._kernel import *
 from ._approximate import *
+# background imports mathLib so don't import background in mathLib
 from ._background import *
-from .background import *
 from ._warpExposure import *
