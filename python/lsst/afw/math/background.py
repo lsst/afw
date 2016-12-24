72c4b295f09e06762887ce44bf39d345a36ee031
Russell Owen <rowen@uw.edu>
2016-12-23 16:29:52 -0800

    More table, schema and function wrapping
    Wrap SpherePoint
    Wrap more of function and fix some Function bugs.
    Wrap more of Schema.
    Fix errors in wrapping ColumnView for various tables.
    Remove test.txt and SConscript code that uses it
    Fix testTableMultiMatch.py
    Fix testSpherePoint.py
    Enable skipped test in testFunction.py
    Enable skipped test in testSpatialCell.py

--- python/lsst/afw/math/background.py
+++ python/lsst/afw/math/background.py
@@ -2,11 +2,11 @@ from __future__ import absolute_import
 
 from ._background import Background
 
-__all__ = []
+__all__ = []  # import this module only for its side effects
+
 
 def __reduce__(self):
     """Pickling"""
     return self.__class__, (self.getImageBBox(), self.getStatsImage())
 
 Background.__reduce__ = __reduce__
-
