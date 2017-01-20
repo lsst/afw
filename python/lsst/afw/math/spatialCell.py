fdc31af444d8b865ed62e3972c7c1f161b9a3b6a
Russell Owen <rowen@uw.edu>
2017-01-20 15:47:52 -0800

    Clean up SpatialCell wrapping
    - Add missing wrappers
    - Improve the python wrapper file as follows:
      - Remove Python implementations of SpatialCell.getCandidateRating
    and setCandidateRating; these were purely intended for testing
    and have long been available in a test subclass.
    They also interfered with wrapping subclasses with pybind11.
      - Modify `__getitem__` use less memory (it retrieves one item
        at a time instead of constructing a list) and support
        negative indices
    - Add a few missing keywords arguments
    - Run clang-format

--- python/lsst/afw/math/spatialCell.py
+++ python/lsst/afw/math/spatialCell.py
@@ -1,6 +1,10 @@
 from __future__ import absolute_import
+
 from lsst.pex.exceptions import NotFoundError
-from ._spatialCell import *
+from ._spatialCell import SpatialCellCandidateIterator, SpatialCell
+
+__all__ = []  # import this module only for its side effects
+
 
 def spatialCellCandidateIter(self):
     while True:
@@ -9,23 +13,22 @@ def spatialCellCandidateIter(self):
         except NotFoundError:
             raise StopIteration
         self.__incr__()
+SpatialCellCandidateIterator.__iter__ = spatialCellCandidateIter
 
-def spatialCellGetitem(self, idx):
-    l = [x for x in self.begin()]
-    return [x for x in self.begin()][idx]
 
 def spatialCellIter(self):
     return self.begin().__iter__()
-
-SpatialCellCandidateIterator.__iter__ = spatialCellCandidateIter
-SpatialCell.__getitem__ = spatialCellGetitem
 SpatialCell.__iter__ = spatialCellIter
 
-def getCandidateRating(self):
-    return self._flux
 
-def setCandidateRating(self, flux):
-    self._flux = flux
-
-SpatialCellCandidate.getCandidateRating = getCandidateRating
-SpatialCellCandidate.setCandidateRating = setCandidateRating
+def spatialCellGetitem(self, idx):
+    idx = int(idx)
+    num_cells = len(self)
+    if idx < -num_cells or idx >= num_cells:
+        raise IndexError("idx={} < -{} or >= {})".format(idx, num_cells, num_cells))
+    if idx < 0:
+        idx += num_cells
+    for i, cell in enumerate(self):
+        if i >= idx:
+            return cell
+SpatialCell.__getitem__ = spatialCellGetitem
