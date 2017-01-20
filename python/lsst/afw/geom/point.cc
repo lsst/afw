e268f87a24185dc58889411c54883bc020d5f95b
Russell Owen <rowen@uw.edu>
2017-01-19 18:15:22 -0800

    Modernize point.cc and extent.cc
    Use `namespace {...`
    Run clang-format (primarily to wrap some overly long lines)

--- python/lsst/afw/geom/point.cc
+++ python/lsst/afw/geom/point.cc
@@ -21,86 +21,95 @@
  */
 
 #include <pybind11/pybind11.h>
-#include <pybind11/operators.h>
 #include "numpy/arrayobject.h"
 #include "ndarray/pybind11.h"
-#include "ndarray/converter.h"
 
 #include "lsst/afw/geom/Point.h"
 #include "lsst/afw/geom/Extent.h"
 
 namespace py = pybind11;
+using namespace pybind11::literals;
 
-using namespace lsst::afw::geom;
+namespace lsst {
+namespace afw {
+namespace geom {
+
+namespace {
 
 template <typename T, int N>
-py::class_<PointBase<T,N>> declarePointBase(py::module &mod, const std::string & suffix) {
+py::class_<PointBase<T, N>> declarePointBase(py::module &mod, const std::string &suffix) {
     const std::string name = "PointBase" + suffix;
-    py::class_<PointBase<T,N>, CoordinateBase<Point<T,N>,T,N>> cls(mod, name.c_str());
+    py::class_<PointBase<T, N>, CoordinateBase<Point<T, N>, T, N>> cls(mod, name.c_str());
 
     /* Operators */
-    cls.def("eq", [](PointBase<T,N> &p, Point<T,N> value) { return p.eq(value); });
-    cls.def("ne", [](PointBase<T,N> &p, Point<T,N> value) { return p.ne(value); });
-    cls.def("lt", [](PointBase<T,N> &p, Point<T,N> value) { return p.lt(value); });
-    cls.def("le", [](PointBase<T,N> &p, Point<T,N> value) { return p.le(value); });
-    cls.def("gt", [](PointBase<T,N> &p, Point<T,N> value) { return p.gt(value); });
-    cls.def("ge", [](PointBase<T,N> &p, Point<T,N> value) { return p.ge(value); });
-    cls.def("eq", [](PointBase<T,N> &p, T value) { return p.eq(value); });
-    cls.def("ne", [](PointBase<T,N> &p, T value) { return p.ne(value); });
-    cls.def("lt", [](PointBase<T,N> &p, T value) { return p.lt(value); });
-    cls.def("le", [](PointBase<T,N> &p, T value) { return p.le(value); });
-    cls.def("gt", [](PointBase<T,N> &p, T value) { return p.gt(value); });
-    cls.def("ge", [](PointBase<T,N> &p, T value) { return p.ge(value); });
+    cls.def("eq", [](PointBase<T, N> &self, Point<T, N> other) { return self.eq(other); });
+    cls.def("ne", [](PointBase<T, N> &self, Point<T, N> other) { return self.ne(other); });
+    cls.def("lt", [](PointBase<T, N> &self, Point<T, N> other) { return self.lt(other); });
+    cls.def("le", [](PointBase<T, N> &self, Point<T, N> other) { return self.le(other); });
+    cls.def("gt", [](PointBase<T, N> &self, Point<T, N> other) { return self.gt(other); });
+    cls.def("ge", [](PointBase<T, N> &self, Point<T, N> other) { return self.ge(other); });
+    cls.def("eq", [](PointBase<T, N> &self, T other) { return self.eq(other); });
+    cls.def("ne", [](PointBase<T, N> &self, T other) { return self.ne(other); });
+    cls.def("lt", [](PointBase<T, N> &self, T other) { return self.lt(other); });
+    cls.def("le", [](PointBase<T, N> &self, T other) { return self.le(other); });
+    cls.def("gt", [](PointBase<T, N> &self, T other) { return self.gt(other); });
+    cls.def("ge", [](PointBase<T, N> &self, T other) { return self.ge(other); });
 
     /* Members */
-    cls.def("asExtent", &PointBase<T,N>::asExtent);
-    cls.def("shift", &PointBase<T,N>::shift);
-    cls.def("scale", &PointBase<T,N>::scale);
-    cls.def("distanceSquared", &PointBase<T,N>::distanceSquared);
-    cls.def("toString", &PointBase<T,N>::toString);
+    cls.def("asExtent", &PointBase<T, N>::asExtent);
+    cls.def("shift", &PointBase<T, N>::shift);
+    cls.def("scale", &PointBase<T, N>::scale);
+    cls.def("distanceSquared", &PointBase<T, N>::distanceSquared);
+    cls.def("toString", &PointBase<T, N>::toString);
 
     return cls;
 }
 
 // Common functionality
 template <typename T, int N>
-py::class_<Point<T,N>> declarePoint(py::module &mod, const std::string & suffix) {
+py::class_<Point<T, N>> declarePoint(py::module &mod, const std::string &suffix) {
     const std::string name = "Point" + suffix;
-    py::class_<Point<T,N>, PointBase<T, N>> cls(mod, name.c_str());
+    py::class_<Point<T, N>, PointBase<T, N>> cls(mod, name.c_str());
 
     /* Constructors */
-    cls.def(py::init<T>(),
-        py::arg("val")=static_cast<T>(0));
+    cls.def(py::init<T>(), "value"_a=static_cast<T>(0));
     // Note that we can't use T here because both types are needed
-    cls.def(py::init<Point<double,N> const &>());
-    cls.def(py::init<Point<int,N> const &>());
-    cls.def(py::init<Extent<T,N> const &>());
+    cls.def(py::init<Point<double, N> const &>());
+    cls.def(py::init<Point<int, N> const &>());
+    cls.def(py::init<Extent<T, N> const &>());
     cls.def(py::init<typename Point<T, N>::EigenVector>());
 
     /* Operators */
-    cls.def(py::self + Extent<double,N>());
-    cls.def(py::self + Extent<int,N>());
-    cls.def("__sub__", [](Point<T,N> &p, Point<T,N> &o) { return p - o; });
-    cls.def("__sub__", [](Point<T,N> &p, Extent<T,N> &o) { return p - o; });
-    cls.def("__sub__", [](Point<T,N> &p, Point<double,N> &o) { return p - o; });
-    cls.def("__sub__", [](Point<T,N> &p, Point<int,N> &o) { return p - o; });
-    cls.def("__sub__", [](Point<T,N> &p, Extent<double,N> &o) { return p - o; });
-    cls.def("__sub__", [](Point<T,N> &p, Extent<int,N> &o) { return p - o; });
-    cls.def("__eq__", [](Point<T,N> &p, Point<T,N> &o) { return p == o; });
-    cls.def("__eq__", [](Point<T,N> &p, py::none) { return false; });
-    cls.def("__ne__", [](Point<T,N> &p, Point<T,N> &o) { return p != o; });
-    cls.def("__ne__", [](Point<T,N> &p, py::none) { return true; });
+    cls.def("__add__", [](Point<T, N> &self, Extent<double, N> &other) { return self + other; },
+            py::is_operator());
+    cls.def("__add__", [](Point<T, N> &self, Extent<int, N> &other) { return self + other; },
+            py::is_operator());
+    cls.def("__sub__", [](Point<T, N> &self, Point<T, N> &other) { return self - other; }, py::is_operator());
+    cls.def("__sub__", [](Point<T, N> &self, Extent<T, N> &other) { return self - other; },
+            py::is_operator());
+    cls.def("__sub__", [](Point<T, N> &self, Point<double, N> &other) { return self - other; },
+            py::is_operator());
+    cls.def("__sub__", [](Point<T, N> &self, Point<int, N> &other) { return self - other; },
+            py::is_operator());
+    cls.def("__sub__", [](Point<T, N> &self, Extent<double, N> &other) { return self - other; },
+            py::is_operator());
+    cls.def("__sub__", [](Point<T, N> &self, Extent<int, N> &other) { return self - other; },
+            py::is_operator());
+    cls.def("__eq__", [](Point<T, N> &self, Point<T, N> &other) { return self == other; }, py::is_operator());
+    cls.def("__eq__", [](Point<T, N> &self, py::none) { return false; }, py::is_operator());
+    cls.def("__ne__", [](Point<T, N> &self, Point<T, N> &other) { return self != other; }, py::is_operator());
+    cls.def("__ne__", [](Point<T, N> &self, py::none) { return true; }, py::is_operator());
 
     /* Members */
-    cls.def("clone", [](Point<T,N> &p) {return Point<T,N>{p};});
+    cls.def("clone", [](Point<T, N> &self) { return Point<T, N>{self}; });
 
     return cls;
 }
 
 // Add functionality only found in N=2 Points
 template <typename T>
-py::class_<Point<T,2>> declarePoint2(py::module &mod, const std::string & suffix) {
-    auto cls = declarePoint<T,2>(mod, std::string("2") + suffix);
+py::class_<Point<T, 2>> declarePoint2(py::module &mod, const std::string &suffix) {
+    auto cls = declarePoint<T, 2>(mod, std::string("2") + suffix);
 
     /* Member types and enums */
     cls.def_property_readonly_static("dimensions", [](py::object /* self */) { return 2; });
@@ -110,18 +119,18 @@ py::class_<Point<T,2>> declarePoint2(py::module &mod, const std::string & suffix
     cls.def(py::init<double, double>());
 
     /* Members */
-    cls.def("getX", [](Point<T,2> &e) { return e[0]; });
-    cls.def("getY", [](Point<T,2> &e) { return e[1]; });
-    cls.def("setX", [](Point<T,2> &e, T value) { e[0] = value; });
-    cls.def("setY", [](Point<T,2> &e, T value) { e[1] = value; });
+    cls.def("getX", [](Point<T, 2> &self) { return self[0]; });
+    cls.def("getY", [](Point<T, 2> &self) { return self[1]; });
+    cls.def("setX", [](Point<T, 2> &self, T other) { self[0] = other; });
+    cls.def("setY", [](Point<T, 2> &self, T other) { self[1] = other; });
 
     return cls;
 }
 
 // Add functionality only found in N=3 Points
 template <typename T>
-py::class_<Point<T,3>> declarePoint3(py::module &mod, const std::string & suffix) {
-    auto cls = declarePoint<T,3>(mod, std::string("3") + suffix);
+py::class_<Point<T, 3>> declarePoint3(py::module &mod, const std::string &suffix) {
+    auto cls = declarePoint<T, 3>(mod, std::string("3") + suffix);
 
     /* Member types and enums */
     cls.def_property_readonly_static("dimensions", [](py::object /* self */) { return 3; });
@@ -131,15 +140,17 @@ py::class_<Point<T,3>> declarePoint3(py::module &mod, const std::string & suffix
     cls.def(py::init<double, double, double>());
 
     /* Members */
-    cls.def("getX", [](Point<T,3> &e) { return e[0]; });
-    cls.def("getY", [](Point<T,3> &e) { return e[1]; });
-    cls.def("getZ", [](Point<T,3> &e) { return e[2]; });
-    cls.def("setX", [](Point<T,3> &e, T value) { e[0] = value; });
-    cls.def("setY", [](Point<T,3> &e, T value) { e[1] = value; });
-    cls.def("setZ", [](Point<T,3> &e, T value) { e[2] = value; });
+    cls.def("getX", [](Point<T, 3> &self) { return self[0]; });
+    cls.def("getY", [](Point<T, 3> &self) { return self[1]; });
+    cls.def("getZ", [](Point<T, 3> &self) { return self[2]; });
+    cls.def("setX", [](Point<T, 3> &self, T other) { self[0] = other; });
+    cls.def("setY", [](Point<T, 3> &self, T other) { self[1] = other; });
+    cls.def("setZ", [](Point<T, 3> &self, T other) { self[2] = other; });
 
     return cls;
 }
+
+}  // namespace lsst::afw::geom::<anonymous>
 
 PYBIND11_PLUGIN(_point) {
     py::module mod("_point", "Python wrapper for afw _point library");
@@ -162,20 +173,58 @@ PYBIND11_PLUGIN(_point) {
     auto clsPoint3D = declarePoint3<double>(mod, "D");
 
     /* Operators */
-    clsPoint2I.def("__iadd__", [](Point<int,2> &p, Extent<int,2> &o) { p += o; return &p; });
-    clsPoint3I.def("__iadd__", [](Point<int,3> &p, Extent<int,3> &o) { p += o; return &p; });
-    clsPoint2D.def("__iadd__", [](Point<double,2> &p, Extent<int,2> &o) { p += o; return &p; });
-    clsPoint3D.def("__iadd__", [](Point<double,3> &p, Extent<int,3> &o) { p += o; return &p; });
-    clsPoint2D.def("__iadd__", [](Point<double,2> &p, Extent<double,2> &o) { p += o; return &p; });
-    clsPoint3D.def("__iadd__", [](Point<double,3> &p, Extent<double,3> &o) { p += o; return &p; });
+    clsPoint2I.def("__iadd__", [](Point<int, 2> &self, Extent<int, 2> &other) {
+        self += other;
+        return &self;
+    });
+    clsPoint3I.def("__iadd__", [](Point<int, 3> &self, Extent<int, 3> &other) {
+        self += other;
+        return &self;
+    });
+    clsPoint2D.def("__iadd__", [](Point<double, 2> &self, Extent<int, 2> &other) {
+        self += other;
+        return &self;
+    });
+    clsPoint3D.def("__iadd__", [](Point<double, 3> &self, Extent<int, 3> &other) {
+        self += other;
+        return &self;
+    });
+    clsPoint2D.def("__iadd__", [](Point<double, 2> &self, Extent<double, 2> &other) {
+        self += other;
+        return &self;
+    });
+    clsPoint3D.def("__iadd__", [](Point<double, 3> &self, Extent<double, 3> &other) {
+        self += other;
+        return &self;
+    });
 
-    clsPoint2I.def("__isub__", [](Point<int,2> &p, Extent<int,2> &o) { p -= o; return &p; });
-    clsPoint3I.def("__isub__", [](Point<int,3> &p, Extent<int,3> &o) { p -= o; return &p; });
-    clsPoint2D.def("__isub__", [](Point<double,2> &p, Extent<int,2> &o) { p -= o; return &p; });
-    clsPoint3D.def("__isub__", [](Point<double,3> &p, Extent<int,3> &o) { p -= o; return &p; });
-    clsPoint2D.def("__isub__", [](Point<double,2> &p, Extent<double,2> &o) { p -= o; return &p; });
-    clsPoint3D.def("__isub__", [](Point<double,3> &p, Extent<double,3> &o) { p -= o; return &p; });
+    clsPoint2I.def("__isub__", [](Point<int, 2> &self, Extent<int, 2> &other) {
+        self -= other;
+        return &self;
+    });
+    clsPoint3I.def("__isub__", [](Point<int, 3> &self, Extent<int, 3> &other) {
+        self -= other;
+        return &self;
+    });
+    clsPoint2D.def("__isub__", [](Point<double, 2> &self, Extent<int, 2> &other) {
+        self -= other;
+        return &self;
+    });
+    clsPoint3D.def("__isub__", [](Point<double, 3> &self, Extent<int, 3> &other) {
+        self -= other;
+        return &self;
+    });
+    clsPoint2D.def("__isub__", [](Point<double, 2> &self, Extent<double, 2> &other) {
+        self -= other;
+        return &self;
+    });
+    clsPoint3D.def("__isub__", [](Point<double, 3> &self, Extent<double, 3> &other) {
+        self -= other;
+        return &self;
+    });
 
     return mod.ptr();
 }
-
+}
+}
+}  // namespace lsst::afw::geom
