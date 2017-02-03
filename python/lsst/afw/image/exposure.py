e8248a69d7dc67b2326407c06f857a19e5757ca7
Jim Bosch <jbosch@astro.princeton.edu>
2017-02-03 18:23:35 -0500

    Modernize Exposure pure-Python extensions.
    This stops using supportSlicing to provide some methods to Exposure.
    This may result in a tiny bit of code duplication, but it actually
    results in better behavior in other places:
     - we don't add __float__ and __int__ methods that can never work
     - we implement __setitem__ in a way that should actually work (needs
       testing).

--- python/lsst/afw/image/exposure.py
+++ python/lsst/afw/image/exposure.py
@@ -1,30 +1,50 @@
 from __future__ import absolute_import, division, print_function
 from future.utils import with_metaclass
+import numpy as np
 
-from abc import ABCMeta
+from lsst.utils import TemplateMeta
 
-from .slicing import supportSlicing
 from ._exposure import ExposureI, ExposureF, ExposureD, ExposureU, ExposureL, makeExposure
 
 __all__ = ["Exposure", "ExposureI", "ExposureF", "ExposureD", "ExposureU", "ExposureL", "makeExposure"]
 
-class Exposure(with_metaclass(ABCMeta, object)):
-    pass
 
-for cls in (ExposureI, ExposureF, ExposureD, ExposureU, ExposureL):
-    Exposure.register(cls)
-    supportSlicing(cls)
+class Exposure(with_metaclass(TemplateMeta, object)):
 
     def __reduce__(self):
         from lsst.afw.fits import reduceToFits
         return reduceToFits(self)
-    cls.__reduce__ = __reduce__
 
     def convertF(self):
-        return ExposureF(self, True)
-    cls.convertF = convertF
+        return ExposureF(self, deep=True)
 
     def convertD(self):
-        return ExposureD(self, True)
-    cls.convertD = convertD
+        return ExposureD(self, deep=True)
 
+    @classmethod
+    def Factory(cls, *args, **kwargs):
+        """Construct a new Exposure of the same dtype.
+        """
+        return cls(*args, **kwargs)
+
+    def clone(self):
+        """Return a deep copy of self"""
+        return self.Factory(self, deep=True)
+
+    def __getitem__(self, imageSlice):
+        return self.Factory(self.getMaskedImage()[imageSlice])
+
+    def __setitem__(self, imageSlice, rhs):
+        self.getMaskedImage[imageSlice] = rhs.getMaskedImage()
+
+
+Exposure.register(np.int32, ExposureI)
+Exposure.register(np.float32, ExposureF)
+Exposure.register(np.float64, ExposureD)
+Exposure.register(np.uint16, ExposureU)
+Exposure.register(np.int64, ExposureL)
+Exposure.alias("I", ExposureI)
+Exposure.alias("F", ExposureF)
+Exposure.alias("D", ExposureD)
+Exposure.alias("U", ExposureU)
+Exposure.alias("L", ExposureL)
