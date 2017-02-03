c326142ebf00ea0f84016cf22964e03f50cd2f4d
Jim Bosch <jbosch@astro.princeton.edu>
2017-02-03 17:55:16 -0500

    Make Exposure return MaskedImage by reference.
    With pybind11 this is now safe, and it finally provides us a way to
    update the Mask's MaskPlaneDict in-place.

--- python/lsst/afw/image/exposure.cc
+++ python/lsst/afw/image/exposure.cc
@@ -113,7 +113,8 @@ PyExposure<PixelT> declareExposure(py::module & mod, const std::string & suffix)
 
 
     /* Members */
-    cls.def("getMaskedImage", (MaskedImageT (ExposureT::*)()) &ExposureT::getMaskedImage);
+    cls.def("getMaskedImage", (MaskedImageT & (ExposureT::*)()) &ExposureT::getMaskedImage,
+            py::return_value_policy::reference_internal);
     cls.def("setMaskedImage", &ExposureT::setMaskedImage, "maskedImage"_a);
     cls.def("getMetadata", &ExposureT::getMetadata);
     cls.def("setMetadata", &ExposureT::setMetadata, "metadata"_a);
