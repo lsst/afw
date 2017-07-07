986947409c26329cc3963b96836fb497b4dab72a
Russell Owen <rowen@uw.edu>
2017-07-07 16:49:34 -0700

    Delete Transform.saveToFile method
    Given the new Transform persistence methods, there is no need
    for saveToFile.

--- python/lsst/afw/geom/python/transform.py
+++ python/lsst/afw/geom/python/transform.py
@@ -66,13 +66,6 @@ def then(self, next):
             % (self, next))
 
 
-def saveToFile(self, path):
-    """Save this @ref pybind11_transform "Transform" to the specified file
-    """
-    with open(path, "w") as outFile:
-        outFile.write(self.writeString())
-
-
 def addTransformMethods(cls):
     """Add pure python methods to the specified Transform class, and register
     the class in `transformRegistry`
@@ -93,4 +86,3 @@ def addTransformMethods(cls):
     transformRegistry[cls.__name__] = cls
     cls.getJacobian = getJacobian
     cls.then = then
-    cls.saveToFile = saveToFile
