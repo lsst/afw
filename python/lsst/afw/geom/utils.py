fb39b4546ca22d3aeaa47e0df07308e923cf191a
Russell Owen <rowen@uw.edu>
2017-12-12 17:05:02 -0800

    Move wcs test code from image to geom

--- python/lsst/afw/geom/utils.py
+++ python/lsst/afw/geom/utils.py
@@ -24,13 +24,10 @@ from __future__ import absolute_import, division, print_function
 
 In the case of the assert functions, importing them makes them available in lsst.utils.tests.TestCase
 """
-__all__ = ["assertAnglesAlmostEqual", "assertPairsAlmostEqual",
-           "assertPairListsAlmostEqual", "assertSpherePointsAlmostEqual",
-           "assertSpherePointListsAlmostEqual", "assertBoxesAlmostEqual",
-           "makeEndpoints", "assertAnglesNearlyEqual", "assertPairsNearlyEqual",
-           "assertBoxesNearlyEqual"]
+__all__ = ["wcsAlmostEqualOverBBox"]
 
 from builtins import range
+import itertools
 import warnings
 import math
 
@@ -38,6 +35,8 @@ import numpy as np
 
 import lsst.utils.tests
 from .angle import arcseconds
+from .box import Box2D
+from .coordinates import Point2D
 from .endpoint import GenericEndpoint, Point2Endpoint, IcrsCoordEndpoint
 
 
@@ -195,6 +194,138 @@ def assertBoxesAlmostEqual(testCase, box0, box1, maxDiff=1e-7, msg="Boxes differ
                            box1.getMin(), maxDiff=maxDiff, msg=msg + ": min")
     assertPairsAlmostEqual(testCase, box0.getMax(),
                            box1.getMax(), maxDiff=maxDiff, msg=msg + ": max")
+
+
+def _compareWcsOverBBox(wcs0, wcs1, bbox, maxDiffSky=0.01*arcseconds,
+                        maxDiffPix=0.01, nx=5, ny=5, doShortCircuit=True):
+    """!Compare two WCS over a rectangular grid of pixel positions
+
+    @param[in] wcs0  WCS 0 (an lsst.afw.geom.SkyWcs)
+    @param[in] wcs1  WCS 1 (an lsst.afw.geom.SkyWcs)
+    @param[in] bbox  boundaries of pixel grid over which to compare the WCSs (an lsst.afw.geom.Box2I or Box2D)
+    @param[in] maxDiffSky  maximum separation between sky positions computed using Wcs.pixelToSky
+        (an lsst.afw.geom.Angle)
+    @param[in] maxDiffPix  maximum separation between pixel positions computed using Wcs.skyToPixel
+    @param[in] nx  number of points in x for the grid of pixel positions
+    @param[in] ny  number of points in y for the grid of pixel positions
+    @param[in] doShortCircuit  if True then stop at the first error, else test all values in the grid
+        and return information about the worst violations found
+
+    @return return an empty string if the WCS are sufficiently close; else return a string describing
+    the largest error measured in pixel coordinates (if sky to pixel error was excessive) and sky coordinates
+    (if pixel to sky error was excessive). If doShortCircuit is true then the reported error is likely to be
+    much less than the maximum error across the whole pixel grid.
+    """
+    if nx < 1 or ny < 1:
+        raise RuntimeError(
+            "nx = %s and ny = %s must both be positive" % (nx, ny))
+    if maxDiffSky <= 0*arcseconds:
+        raise RuntimeError("maxDiffSky = %s must be positive" % (maxDiffSky,))
+    if maxDiffPix <= 0:
+        raise RuntimeError("maxDiffPix = %s must be positive" % (maxDiffPix,))
+
+    bboxd = Box2D(bbox)
+    xList = np.linspace(bboxd.getMinX(), bboxd.getMaxX(), nx)
+    yList = np.linspace(bboxd.getMinY(), bboxd.getMaxY(), ny)
+    # we don't care about measured error unless it is too large, so initialize
+    # to max allowed
+    measDiffSky = (maxDiffSky, "?")  # (sky diff, pix pos)
+    measDiffPix = (maxDiffPix, "?")  # (pix diff, sky pos)
+    for x, y in itertools.product(xList, yList):
+        fromPixPos = Point2D(x, y)
+        sky0 = wcs0.pixelToSky(fromPixPos)
+        sky1 = wcs1.pixelToSky(fromPixPos)
+        diffSky = sky0.angularSeparation(sky1)
+        if diffSky > measDiffSky[0]:
+            measDiffSky = (diffSky, fromPixPos)
+            if doShortCircuit:
+                break
+
+        toPixPos0 = wcs0.skyToPixel(sky0)
+        toPixPos1 = wcs1.skyToPixel(sky0)
+        diffPix = math.hypot(*(toPixPos0 - toPixPos1))
+        if diffPix > measDiffPix[0]:
+            measDiffPix = (diffPix, sky0)
+            if doShortCircuit:
+                break
+
+    msgList = []
+    if measDiffSky[0] > maxDiffSky:
+        msgList.append("%s arcsec max measured sky error > %s arcsec max allowed sky error at pix pos=%s" %
+                       (measDiffSky[0].asArcseconds(), maxDiffSky.asArcseconds(), measDiffSky[1]))
+    if measDiffPix[0] > maxDiffPix:
+        msgList.append("%s max measured pix error > %s max allowed pix error at sky pos=%s" %
+                       (measDiffPix[0], maxDiffPix, measDiffPix[1]))
+
+    return "; ".join(msgList)
+
+
+def wcsAlmostEqualOverBBox(wcs0, wcs1, bbox, maxDiffSky=0.01*arcseconds,
+                           maxDiffPix=0.01, nx=5, ny=5):
+    """!Return True if two WCS are almost equal over a grid of pixel positions, else False
+
+    @param[in] wcs0  WCS 0 (an lsst.afw.geom.SkyWcs)
+    @param[in] wcs1  WCS 1 (an lsst.afw.geom.SkyWcs)
+    @param[in] bbox  boundaries of pixel grid over which to compare the WCSs (an lsst.afw.geom.Box2I or Box2D)
+    @param[in] maxDiffSky  maximum separation between sky positions computed using Wcs.pixelToSky
+        (an lsst.afw.geom.Angle)
+    @param[in] maxDiffPix  maximum separation between pixel positions computed using Wcs.skyToPixel
+    @param[in] nx  number of points in x for the grid of pixel positions
+    @param[in] ny  number of points in y for the grid of pixel positions
+    """
+    return not bool(_compareWcsOverBBox(
+        wcs0 = wcs0,
+        wcs1 = wcs1,
+        bbox = bbox,
+        maxDiffSky = maxDiffSky,
+        maxDiffPix = maxDiffPix,
+        nx = nx,
+        ny = ny,
+        doShortCircuit = True,
+    ))
+
+
+@lsst.utils.tests.inTestCase
+def assertWcsAlmostEqualOverBBox(testCase, wcs0, wcs1, bbox, maxDiffSky=0.01*arcseconds,
+                                 maxDiffPix=0.01, nx=5, ny=5, msg="WCSs differ"):
+    """!Assert that two WCS are almost equal over a grid of pixel positions
+
+    Compare pixelToSky and skyToPixel for two WCS over a rectangular grid of pixel positions.
+    If the WCS are too divergent at any point, call testCase.fail; the message describes
+    the largest error measured in pixel coordinates (if sky to pixel error was excessive)
+    and sky coordinates (if pixel to sky error was excessive) across the entire pixel grid.
+
+    @param[in] testCase  unittest.TestCase instance the test is part of;
+                        an object supporting one method: fail(self, msgStr)
+    @param[in] wcs0  WCS 0 (an lsst.afw.geom.SkyWcs)
+    @param[in] wcs1  WCS 1 (an lsst.afw.geom.SkyWcs)
+    @param[in] bbox  boundaries of pixel grid over which to compare the WCSs (an lsst.afw.geom.Box2I or Box2D)
+    @param[in] maxDiffSky  maximum separation between sky positions computed using Wcs.pixelToSky
+        (an lsst.afw.geom.Angle)
+    @param[in] maxDiffPix  maximum separation between pixel positions computed using Wcs.skyToPixel
+    @param[in] nx  number of points in x for the grid of pixel positions
+    @param[in] ny  number of points in y for the grid of pixel positions
+    @param[in] msg  exception message prefix; details of the error are appended after ": "
+    """
+    errMsg = _compareWcsOverBBox(
+        wcs0 = wcs0,
+        wcs1 = wcs1,
+        bbox = bbox,
+        maxDiffSky = maxDiffSky,
+        maxDiffPix = maxDiffPix,
+        nx = nx,
+        ny = ny,
+        doShortCircuit = False,
+    )
+    if errMsg:
+        testCase.fail("%s: %s" % (msg, errMsg))
+
+
+@lsst.utils.tests.inTestCase
+def assertWcsNearlyEqualOverBBox(*args, **kwargs):
+    warnings.warn("Deprecated. Use assertWcsAlmostEqualOverBBox",
+                  DeprecationWarning, 2)
+    assertWcsAlmostEqualOverBBox(*args, **kwargs)
 
 
 @lsst.utils.tests.inTestCase
