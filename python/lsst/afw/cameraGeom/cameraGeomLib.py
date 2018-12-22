2af942ee7c6bf04398f2069c35540b17e1c567bf
John Parejko <parejkoj@uw.edu>
2018-12-21 16:26:16 -0800

    Remove unneeded import

--- python/lsst/afw/cameraGeom/cameraGeomLib.py
+++ python/lsst/afw/cameraGeom/cameraGeomLib.py
@@ -19,7 +19,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from lsst.afw.geom import Point2D
 from .cameraSys import *
 from .detector import *
 from .orientation import *
