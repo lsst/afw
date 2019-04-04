7f041bb6429047bb6c43e9fef1b8edc3ba2121a8
Jim Bosch <jbosch@astro.princeton.edu>
2019-04-03 21:33:46 -0400

    Add swap implementation for CameraSys.

--- include/lsst/afw/cameraGeom/CameraSys.h
+++ include/lsst/afw/cameraGeom/CameraSys.h
@@ -104,6 +104,11 @@ public:
     CameraSys &operator=(CameraSys const &) = default;
     CameraSys &operator=(CameraSys &&) noexcept = default;
 
+    void swap(CameraSys & other) noexcept {
+        std::swap(_sysName, other._sysName);
+        std::swap(_detectorName, other._detectorName);
+    }
+
     /**
      * Get coordinate system name
      */
@@ -148,6 +153,11 @@ private:
     std::string _sysName;       ///< coordinate system name
     std::string _detectorName;  ///< detector name; "" if not a detector-specific coordinate system
 };
+
+inline void swap(CameraSys & a, CameraSys & b) {
+    a.swap(b);
+}
+
 
 // *** Standard camera coordinate systems ***
 
@@ -216,6 +226,11 @@ std::ostream &operator<<(std::ostream &os, CameraSys const &cameraSys);
 }  // namespace lsst
 
 namespace std {
+
+inline void swap(lsst::afw::cameraGeom::CameraSys & a, lsst::afw::cameraGeom::CameraSys & b) noexcept {
+    a.swap(b);
+}
+
 template <>
 struct hash<lsst::afw::cameraGeom::CameraSysPrefix> {
     using argument_type = lsst::afw::cameraGeom::CameraSysPrefix;
