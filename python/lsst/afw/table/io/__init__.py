d2f3892a2700ec05aaaa8ebf1ac2b5d48369c754
Krzysztof Findeisen <kfindeis@uw.edu>
2019-07-10 15:33:50 -0700

    Add empty WrapperCollection for afw.table.io.

--- python/lsst/afw/table/io/__init__.py
+++ python/lsst/afw/table/io/__init__.py
@@ -19,5 +19,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+from ._io import *
 from .persistable import *
 from .fits import *
