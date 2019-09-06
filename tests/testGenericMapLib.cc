5e93c1b49f883edebe267d031be4c1a083f13af6
Krzysztof Findeisen <kfindeis@uw.edu>
2019-09-06 12:10:44 -0700

    Create empty pybind11 test module for GenericMap.

---
+++ tests/testGenericMapLib.cc
@@ -0,0 +1,35 @@
+/*
+ * This file is part of afw.
+ *
+ * Developed for the LSST Data Management System.
+ * This product includes software developed by the LSST Project
+ * (https://www.lsst.org).
+ * See the COPYRIGHT file at the top-level directory of this distribution
+ * for details of code ownership.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <pybind11/pybind11.h>
+
+#include <memory>
+
+#include "lsst/afw/typehandling/GenericMap.h"
+#include "lsst/afw/typehandling/Storable.h"
+
+namespace py = pybind11;
+
+namespace {}  // namespace
+
+PYBIND11_MODULE(testGenericMapLib, mod) { py::module::import("lsst.afw.typehandling"); }
