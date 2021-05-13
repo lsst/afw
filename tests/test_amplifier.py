b82bf3da7a14a3cc463f1260decba6698824c53d
Jim Bosch <jbosch@astro.princeton.edu>
2021-05-13 13:11:38 -0400

    Add tests for new Amplifier and Amplifier.Builder methods.

--- tests/test_amplifier.py
+++ tests/test_amplifier.py
@@ -25,10 +25,11 @@ import numpy as np
 
 import lsst.utils.tests
 import lsst.geom
-from lsst.afw.cameraGeom import Amplifier, ReadoutCorner
+from lsst.afw.cameraGeom import Amplifier, AmplifierGeometryComparison, ReadoutCorner
+import lsst.afw.cameraGeom.testUtils  # for assert methods injected into TestCase
 
 
-class AmplifierTestCase(unittest.TestCase):
+class AmplifierTestCase(lsst.utils.tests.TestCase):
 
     def setUp(self):
         self.data = SimpleNamespace(
@@ -127,6 +128,113 @@ class AmplifierTestCase(unittest.TestCase):
         amplifier = builder.finish()
         self.assertEqual(newPrescanBBox2,
                          amplifier.getRawPrescanBBox())
+
+    def test_compareGeometry(self):
+        """Test the `Amplifier.compareGeometry` method.
+
+        This doesn't handle the case where the amplifiers have the same
+        regions but different assembly state (and hence the regions look
+        different until they are transformed).  That test is in test_transform.
+        """
+        Cmp = AmplifierGeometryComparison
+
+        def test_combos(rhs, expected):
+            """Test all combinations of flag kwargs to compareGeometry.
+
+            Parameters
+            ----------
+            rhs : `Amplifier`
+                RHS of comparison (LHS is always self.amplifier).
+            expected : `list` of `AmplifierGeometryComparison`
+                Expected results for comparison (just look at the code for
+                order).
+            """
+            self.assertIs(self.amplifier.compareGeometry(rhs), expected[0])
+            self.assertIs(self.amplifier.compareGeometry(rhs, assembly=False), expected[1])
+            self.assertIs(self.amplifier.compareGeometry(rhs, regions=False), expected[2])
+            self.assertIs(self.amplifier.compareGeometry(rhs, assembly=False, regions=False), Cmp.EQUAL)
+
+        def modified(**kwargs):
+            """Return an Amplifier by modifying a copy of ``self.amplifier``.
+
+            Keyword arguments encode the name of an `Amplifier.Builder` setter
+            method in the key and the value for that setter in the value.
+            """
+            builder = self.amplifier.rebuild()
+            for k, v in kwargs.items():
+                getattr(builder, k)(v)
+            return builder.rebuild()
+
+        # Comparing an amplifier to itself always returns EQUAL.
+        test_combos(self.amplifier, [Cmp.EQUAL]*4)
+        # If we modify a flip or shift parameter we notice iff assembly=true.
+        # If regions=True as well, then those compare as different because
+        # the method tries to transform to the same assembly state and that
+        # makes them different.
+        test_combos(modified(setRawFlipX=not self.data.rawFlipX),
+                    [Cmp.FLIPPED_X | Cmp.REGIONS_DIFFER, Cmp.EQUAL, Cmp.FLIPPED_X])
+        test_combos(modified(setRawFlipY=not self.data.rawFlipY),
+                    [Cmp.FLIPPED_Y | Cmp.REGIONS_DIFFER, Cmp.EQUAL, Cmp.FLIPPED_Y])
+        test_combos(modified(setRawXYOffset=self.data.rawXYOffset + lsst.geom.Extent2I(4, 5)),
+                    [Cmp.SHIFTED_X | Cmp.SHIFTED_Y | Cmp.REGIONS_DIFFER, Cmp.EQUAL,
+                     Cmp.SHIFTED_X | Cmp.SHIFTED_Y])
+        # If we modify or data box we notice iff regions=True.  We modify them
+        # all by shifting by the same amount because the Amplifier constructor
+        # would be within its rights to raise if these didn't line up properly,
+        # and nontrivial modifications that are valid are a lot harder to
+        # write.
+        offset = lsst.geom.Extent2I(-2, 3)
+        test_combos(
+            modified(
+                setRawBBox=self.data.rawBBox.shiftedBy(offset),
+                setRawDataBBox=self.data.rawDataBBox.shiftedBy(offset),
+                setRawHorizontalOverscanBBox=self.data.rawHorizontalOverscanBBox.shiftedBy(offset),
+                setRawVerticalOverscanBBox=self.data.rawVerticalOverscanBBox.shiftedBy(offset),
+                setRawPrescanBBox=self.data.rawPrescanBBox.shiftedBy(offset),
+            ),
+            [Cmp.REGIONS_DIFFER, Cmp.REGIONS_DIFFER, Cmp.EQUAL, Cmp.EQUAL],
+        )
+
+    def test_transform(self):
+        """Test the `Amplifier.Builder.transform` method and
+        `Amplifier.compareGeometry` cases that involve post-transform
+        comparisons.
+        """
+        # Standard case: no kwargs to transform, so we apply flips and
+        # rawXYOffset shifts.  This should leave no flip or offset in the
+        # returned amplifier.
+        assembled_amp = self.amplifier.rebuild().transform().finish()
+        self.assertFalse(assembled_amp.getRawFlipX())
+        self.assertFalse(assembled_amp.getRawFlipY())
+        self.assertEqual(assembled_amp.getRawXYOffset(), lsst.geom.Extent2I(0, 0))
+        comparison = self.amplifier.compareGeometry(assembled_amp)
+        self.assertEqual(bool(comparison & comparison.FLIPPED_X), self.data.rawFlipX)
+        self.assertEqual(bool(comparison & comparison.FLIPPED_Y), self.data.rawFlipY)
+        self.assertEqual(bool(comparison & comparison.SHIFTED),
+                         self.data.rawXYOffset != lsst.geom.Extent2I(0, 0))
+        self.assertFalse(comparison & comparison.REGIONS_DIFFER)
+        # Use transform to round-trip the assembled amp back to the original.
+        roundtripped = assembled_amp.rebuild().transform(
+            outOffset=self.data.rawXYOffset,
+            outFlipX=self.data.rawFlipX,
+            outFlipY=self.data.rawFlipY,
+        ).finish()
+        self.assertAmplifiersEqual(self.amplifier, roundtripped)
+        self.assertIs(self.amplifier.compareGeometry(roundtripped), AmplifierGeometryComparison.EQUAL)
+        # Transform to completely different offsets and the inverse of the
+        # flips we tried before, just to make sure we didn't mix up X and Y
+        # somewhere.
+        different = self.amplifier.rebuild().transform(
+            outOffset=lsst.geom.Extent2I(7, 8),
+            outFlipX=not self.data.rawFlipX,
+            outFlipY=not self.data.rawFlipY,
+        ).finish()
+        self.assertEqual(different.getRawFlipX(), not self.data.rawFlipX)
+        self.assertEqual(different.getRawFlipY(), not self.data.rawFlipY)
+        self.assertEqual(different.getRawXYOffset(), lsst.geom.Extent2I(7, 8))
+        comparison = self.amplifier.compareGeometry(different)
+        self.assertTrue(comparison & comparison.ASSEMBLY_DIFFERS)
+        self.assertFalse(comparison & comparison.REGIONS_DIFFER)
 
 
 class TestMemory(lsst.utils.tests.MemoryTestCase):
