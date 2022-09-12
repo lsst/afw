e448edd8b6dd2e98c9c6fce6c77f986d57f51e51
Dan Weatherill <daniel.weatherill@physics.ox.ac.uk>
2022-09-12 20:38:38 -0300

    fix test failure - wrong filepath & arg names

--- tests/test_fits.py
+++ tests/test_fits.py
@@ -129,20 +129,20 @@ class FitsTestCase(lsst.utils.tests.TestCase):
         self.assertEqual(metadata.getArray("FOO"), [None, None])
 
     def testNamedHeaderNavigate(self):
-        testfile = os.path.join("data", "multi_extension_metadata.fits")
+        testfile = os.path.join(testPath, "data", "multi_extension_metadata.fits")
 
         # load metadata from the extra table header
-        md_extra_tab = lsst.afw.fits.readMetadata(testfile, hduname="EXTRA_TAB")
+        md_extra_tab = lsst.afw.fits.readMetadata(testfile, hduName="EXTRA_TAB")
         # assert the value we put in is in the read metadata
         self.assertTrue("FVALUE" in md_extra_tab)
 
         # load metadata from the extra image header, do same test
-        md_extra_im = lsst.afw.fits.readMetadata(testfile, hduname="EXTRA_IM")
+        md_extra_im = lsst.afw.fits.readMetadata(testfile, hduName="EXTRA_IM")
         self.assertTrue("BLORP" in md_extra_im)
 
         # now try to load a non-existent named HDU and check that we throw
         with self.assertRaises(lsst.afw.fits.FitsError):
-            lsst.afw.fits.readMetadata(testfile, hduname="CORDON_BLEAU")
+            lsst.afw.fits.readMetadata(testfile, hduName="CORDON_BLEAU")
 
 
 class TestMemory(lsst.utils.tests.MemoryTestCase):
