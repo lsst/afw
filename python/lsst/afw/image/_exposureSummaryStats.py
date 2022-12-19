cba4f385d6386fc49d89e0d16f94363fb2f275b1
Jim Bosch <jbosch@astro.princeton.edu>
2022-12-19 14:44:07 -0500

    Relocate exposure summary stats <-> afw.table code here.
    ...from lsst.pipe.tasks.ConsolidateVisitSummaryTask, which now no
    longer depends on the details of what's in the summary stats.
    
    The schema creation code is essentially unchanged; it duplicates all
    field names and docs because I couldn't find a way to extract the
    docstrings from the attributes that isn't worse than the duplication.
    
    The update_record code is rewritten to use dataclasses.fields and
    hence not depend on the fields in too much detail, and there's now
    a from_record method for the opposite direction.

--- python/lsst/afw/image/_exposureSummaryStats.py
+++ python/lsst/afw/image/_exposureSummaryStats.py
@@ -20,22 +20,24 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 from __future__ import annotations
 
-from dataclasses import dataclass, asdict, field
-from typing import List
+import dataclasses
+from typing import TYPE_CHECKING
 import yaml
 import warnings
 
 from ..typehandling import Storable, StorableHelperFactory
 
+if TYPE_CHECKING:
+    from ..table import BaseRecord, Schema
 
 __all__ = ("ExposureSummaryStats", )
 
 
 def _default_corners():
-    return [float('nan')]*4
+    return [float("nan")] * 4
 
 
-@dataclass
+@dataclasses.dataclass
 class ExposureSummaryStats(Storable):
     _persistence_name = 'ExposureSummaryStats'
 
@@ -79,10 +81,10 @@ class ExposureSummaryStats(Storable):
     meanVar: float = float('nan')
     """Mean variance of the weight plane (ADU**2)."""
 
-    raCorners: List[float] = field(default_factory=_default_corners)
+    raCorners: list[float] = dataclasses.field(default_factory=_default_corners)
     """Right Ascension of bounding box corners (degrees)."""
 
-    decCorners: List[float] = field(default_factory=_default_corners)
+    decCorners: list[float] = dataclasses.field(default_factory=_default_corners)
     """Declination of bounding box corners (degrees)."""
 
     astromOffsetMean: float = float('nan')
@@ -128,7 +130,7 @@ class ExposureSummaryStats(Storable):
         return __name__
 
     def _write(self):
-        return yaml.dump(asdict(self), encoding='utf-8')
+        return yaml.dump(dataclasses.asdict(self), encoding='utf-8')
 
     @staticmethod
     def _read(bytes):
@@ -141,7 +143,164 @@ class ExposureSummaryStats(Storable):
                 droppedFields.append(_field)
                 yamlDict.pop(_field)
         if len(droppedFields) > 0:
-            droppedFieldString = ', '.join([str(f) for f in droppedFields])
-            warnings.warn((f"Could not read summary fields [{droppedFieldString}]. "
-                           "Please use a newer stack."), FutureWarning)
+            droppedFieldString = ", ".join([str(f) for f in droppedFields])
+            warnings.warn(
+                (
+                    f"Could not read summary fields [{droppedFieldString}]. "
+                    "Please use a newer stack."
+                ),
+                FutureWarning,
+            )
         return ExposureSummaryStats(**yamlDict)
+
+    @classmethod
+    def update_schema(cls, schema: Schema) -> None:
+        """Update an schema to includes for all summary statistic fields.
+
+        Parameters
+        -------
+        schema : `lsst.afw.table.Schema`
+            Schema to add which fields will be added.
+        """
+        schema.addField(
+            "psfSigma",
+            type="F",
+            doc="PSF model second-moments determinant radius (center of chip) (pixel)",
+        )
+        schema.addField(
+            "psfArea",
+            type="F",
+            doc="PSF model effective area (center of chip) (pixel**2)",
+        )
+        schema.addField(
+            "psfIxx", type="F", doc="PSF model Ixx (center of chip) (pixel**2)"
+        )
+        schema.addField(
+            "psfIyy", type="F", doc="PSF model Iyy (center of chip) (pixel**2)"
+        )
+        schema.addField(
+            "psfIxy", type="F", doc="PSF model Ixy (center of chip) (pixel**2)"
+        )
+        schema.addField(
+            "raCorners",
+            type="ArrayD",
+            size=4,
+            doc="Right Ascension of bounding box corners (degrees)",
+        )
+        schema.addField(
+            "decCorners",
+            type="ArrayD",
+            size=4,
+            doc="Declination of bounding box corners (degrees)",
+        )
+        schema.addField(
+            "ra", type="D", doc="Right Ascension of bounding box center (degrees)"
+        )
+        schema.addField(
+            "decl", type="D", doc="Declination of bounding box center (degrees)"
+        )
+        schema.addField(
+            "zenithDistance",
+            type="F",
+            doc="Zenith distance of bounding box center (degrees)",
+        )
+        schema.addField("zeroPoint", type="F", doc="Mean zeropoint in detector (mag)")
+        schema.addField("skyBg", type="F", doc="Average sky background (ADU)")
+        schema.addField("skyNoise", type="F", doc="Average sky noise (ADU)")
+        schema.addField(
+            "meanVar", type="F", doc="Mean variance of the weight plane (ADU**2)"
+        )
+        schema.addField(
+            "astromOffsetMean",
+            type="F",
+            doc="Mean offset of astrometric calibration matches (arcsec)",
+        )
+        schema.addField(
+            "astromOffsetStd",
+            type="F",
+            doc="Standard deviation of offsets of astrometric calibration matches (arcsec)",
+        )
+        schema.addField("nPsfStar", type="I", doc="Number of stars used for PSF model")
+        schema.addField(
+            "psfStarDeltaE1Median",
+            type="F",
+            doc="Median E1 residual (starE1 - psfE1) for psf stars",
+        )
+        schema.addField(
+            "psfStarDeltaE2Median",
+            type="F",
+            doc="Median E2 residual (starE2 - psfE2) for psf stars",
+        )
+        schema.addField(
+            "psfStarDeltaE1Scatter",
+            type="F",
+            doc="Scatter (via MAD) of E1 residual (starE1 - psfE1) for psf stars",
+        )
+        schema.addField(
+            "psfStarDeltaE2Scatter",
+            type="F",
+            doc="Scatter (via MAD) of E2 residual (starE2 - psfE2) for psf stars",
+        )
+        schema.addField(
+            "psfStarDeltaSizeMedian",
+            type="F",
+            doc="Median size residual (starSize - psfSize) for psf stars (pixel)",
+        )
+        schema.addField(
+            "psfStarDeltaSizeScatter",
+            type="F",
+            doc="Scatter (via MAD) of size residual (starSize - psfSize) for psf stars (pixel)",
+        )
+        schema.addField(
+            "psfStarScaledDeltaSizeScatter",
+            type="F",
+            doc="Scatter (via MAD) of size residual scaled by median size squared",
+        )
+
+    def update_record(self, record: BaseRecord) -> None:
+        """Write summary-statistic columns into a record.
+
+        Parameters
+        ----------
+        record : `lsst.afw.table.BaseRecord`
+            Record to update.  This is expected to frequently be an
+            `ExposureRecord` instance (with higher-level code adding other
+            columns and objects), but this method can work with any record
+            type.
+        """
+        for field in dataclasses.fields(self):
+            value = getattr(self, field.name)
+            if field.name == "version":
+                continue
+            elif field.type.startswith("list"):
+                record[field.name][:] = value
+            else:
+                record[field.name] = value
+
+    @classmethod
+    def from_record(cls, record: BaseRecord) -> ExposureSummaryStats:
+        """Read summary-statistic columns from a record into ``self``.
+
+        Parameters
+        ----------
+        record : `lsst.afw.table.BaseRecord`
+            Record to read from.  This is expected to frequently be an
+            `ExposureRecord` instance (with higher-level code adding other
+            columns and objects), but this method can work with any record
+            type, ignoring any attributes or columns it doesn't recognize.
+
+        Returns
+        -------
+        summary : `ExposureSummaryStats`
+            Summary statistics object created from the given record.
+        """
+        return cls(
+            **{
+                field.name: (
+                    record[field.name] if not field.type.startswith("list")
+                    else [float(v) for v in record[field.name]]
+                )
+                for field in dataclasses.fields(cls)
+                if field.name != "version"
+            }
+        )
