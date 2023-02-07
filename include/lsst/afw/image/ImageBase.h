696682c0d8e780fa8ea79ea9a933ab6eacfb87f3
Matthias Wittgen <matthias.michael.wittgen@cern.ch>
2023-02-06 16:04:26 -0800

    Exposure/BackgroundControl classes don't need a virtual dtor

--- include/lsst/afw/image/ImageBase.h
+++ include/lsst/afw/image/ImageBase.h
@@ -239,7 +239,7 @@ public:
     explicit ImageBase(Array const& array, bool deep = false,
                        lsst::geom::Point2I const& xy0 = lsst::geom::Point2I());
 
-    ~ImageBase() = default;
+    virtual ~ImageBase() = default;
     /** Shallow assignment operator.
      *
      * @note that this has the effect of making the lhs share pixels with the rhs which may
