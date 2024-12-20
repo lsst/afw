889b358c3f32875c6ea1a0cc741f7252f321c4d3
Dan Taranu <dtaranu@astro.princeton.edu>
2024-12-19 16:34:07 -0800

    Add testFootprintSetPeaks with negative origin bbox

--- tests/test_footprint2.py
+++ tests/test_footprint2.py
@@ -173,6 +173,22 @@ class FootprintSetTestCase(unittest.TestCase):
                         i + 1,
                     )
 
+    def testFootprintSetPeaks(self):
+        """Check that peak finding returns separate peaks with a negative
+        origin bbox."""
+
+        # Only the negative y-origin triggers the bug fixed in DM-48092,
+        # but the x may as well also be negative to test regression.
+        img_peaks = afwImage.ImageU(
+            bbox=lsst.geom.Box2I(lsst.geom.Point2I(-10, -3), lsst.geom.Extent2I(5, 6)),
+            initialValue=0,
+        )
+        img_peaks.array[1, 1:4] = [10, 5, 10]
+
+        footprints = afwDetect.FootprintSet(img_peaks, afwDetect.Threshold(5)).getFootprints()
+        self.assertEqual(len(footprints), 1)
+        self.assertEqual(len(footprints[0].getPeaks()), 2)
+
     def testFootprintsImage(self):
         """Check that we can search Images as well as MaskedImages"""
         ds = afwDetect.FootprintSet(self.im, afwDetect.Threshold(10))
