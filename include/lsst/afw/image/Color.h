1f9c513b4a87edb2183113ec9a7a0801d663188c
Pierre-Francois Leget <leget@astro.princeton.edu>
2025-03-19 14:54:26 -0700

    Add access to color value and type.

--- include/lsst/afw/image/Color.h
+++ include/lsst/afw/image/Color.h
@@ -8,6 +8,7 @@
 
 #include <cmath>
 #include <limits>
+#include <string>
 
 namespace lsst {
 namespace afw {
@@ -16,15 +17,20 @@ namespace image {
 /**
  * Describe the colour of a source
  *
- * We need a concept of colour more general than "g - r" in order to calculate e.g. atmospheric dispersion
- * or a source's PSF
+ * Inputs are color type (a string, for exemple 'g-r') and its color value (float).
  *
- * @note This is very much just a place holder until we work out what we need.  A full SED may be required,
- * in which case a constructor from an SED name might be appropriate, or a couple of colours, or ...
  */
 class Color final {
 public:
-    explicit Color(double g_r = std::numeric_limits<double>::quiet_NaN()) : _g_r(g_r) {}
+    /// Default: indeterminate color
+    Color() noexcept
+        : _color_value(std::numeric_limits<double>::quiet_NaN()),
+        _color_type(),
+        _indeterminate(true) {}
+
+    /// Fully-specified color: both a numeric value and its type string
+    Color(double color_value, std::string const & color_type) noexcept
+        : _color_value(color_value), _color_type(color_type), _indeterminate(false) {}
 
     Color(Color const &) = default;
     Color(Color &&) = default;
@@ -32,43 +38,40 @@ public:
     Color &operator=(Color &&) = default;
     ~Color() noexcept = default;
 
-    /// Whether the color is the special value that indicates that it is unspecified.
-    bool isIndeterminate() const noexcept { return std::isnan(_g_r); }
+    /// Whether this Color was default‑constructed (i.e. has no value/type).
+    bool isIndeterminate() const noexcept { return _indeterminate; }
 
-    //@{
+    /// The numeric color value; only valid if !isIndeterminate().
+    double getColorValue() const noexcept { return _color_value; }
+
+    /// The color type string (e.g. "g-r"); only valid if !isIndeterminate().
+    std::string const & getColorType() const noexcept { return _color_type; }
+
     /**
-     *  Equality comparison for colors
+     * Equality comparison for colors.
      *
-     *  Just a placeholder like everything else, but we explicitly let indeterminate colors compare
-     *  as equal.
-     *
-     *  In the future, we'll probably want some way of doing fuzzy comparisons on colors, but then
-     *  we'd have to define some kind of "color difference" matric, and it's not worthwhile doing
-     *  that yet.
+     * Indeterminate colors compare equal to each other; fully-specified
+     * colors compare by both value and type.
      */
-    bool operator==(Color const &other) const noexcept {
-        return (isIndeterminate() && other.isIndeterminate()) || other._g_r == _g_r;
+    bool operator==(Color const & other) const noexcept {
+        if (_indeterminate && other._indeterminate) {
+            return true;
+        }
+        if (_indeterminate != other._indeterminate) {
+            return false;
+        }
+        return _color_value == other._color_value && _color_type == other._color_type;
     }
-    bool operator!=(Color const &other) const noexcept { return !operator==(other); }
-    //@}
-
-    /// Return a hash of this object.
-    std::size_t hash_value() const noexcept { return isIndeterminate() ? 42 : std::hash<double>()(_g_r); }
+    bool operator!=(Color const & other) const noexcept { return !(*this == other); }
 
 private:
-    double _g_r;
+    double      _color_value;
+    std::string _color_type;
+    bool        _indeterminate;
 };
+
 }  // namespace image
 }  // namespace afw
 }  // namespace lsst
-
-namespace std {
-template <>
-struct hash<lsst::afw::image::Color> {
-    using argument_type = lsst::afw::image::Color;
-    using result_type = size_t;
-    size_t operator()(argument_type const &obj) const noexcept { return obj.hash_value(); }
-};
-}  // namespace std
 
 #endif
